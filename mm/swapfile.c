aed2aa7b58eccb9ec30269c96fcd9ab35a928d73
Will Drewry <wad@chromium.org>
2013-03-21 17:00:53 -0500

    CHROMIUM: Restrict swapon() to "zram" devices / lock down zram
    This cl contains three distinct changes collapsed into
    one CL as per semenzato's request:
    1. Restrict CONFIG_SWAP to ensure that only zram devices
       may be used.
    2. Restrict zram to disallow open calls, even from root, if
       the device is claimed (e.g., by sys_swapon)
    3. Add swapoff fallback when filp_open fails to use the path lookup.
       I don't believe swapoff needs filp_open() since kern_path()
       provides the data needed without a file object.
    4. Add an open counter to zram to ensure that it is not opened more
       times than swapon(2) will claim it -- twice:
       - blkdev_get and filp_open (for swap_file)
    
    Signed-off-by: Will Drewry <wad@chromium.org>
    TEST=tested on lumpy; swapon, swapoff, change zram during swapon fails, change withotu swapon succeeds.
    BUG=chromium:220974
    
    Change-Id: Ic281a7004a81b2897cf0bf1c5d334351061261f1
    Reviewed-on: https://gerrit.chromium.org/gerrit/58780
    
    [rebase66(tzungbi):
     Revised commit message.
     Squashed:
       CHROMIUM: mm: allow for non-zram disk-based swap
         (https://crrev.com/c/4926929)
       CHROMIUM: swap: Fix swapfile lookup when file_open_name fails.
         (https://crrev.com/c/3949894)
       FIXUP: CHROMIUM: Restrict swapon() to zram devices lock down zram
    ]
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    
    Kcr-patch: 023f0dda7ccada86de0f1329900c0c9da999f5b7a90f2f89ed4fe575.patch
    Change-Id: I1a762a6a057e7563c86aaf6915296f908f736e92

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2517,6 +2517,8 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	struct inode *inode;
 	struct filename *pathname;
 	int err, found = 0;
+	struct path path_holder;
+	struct path *victim_path = NULL;
 
 	if (!capable(CAP_SYS_ADMIN))
 		return -EPERM;
@@ -2529,14 +2531,25 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 
 	victim = file_open_name(pathname, O_RDWR|O_LARGEFILE, 0);
 	err = PTR_ERR(victim);
-	if (IS_ERR(victim))
-		goto out;
-
-	mapping = victim->f_mapping;
+	if (IS_ERR(victim)) {
+		/* Fallback to just the inode mapping if possible. */
+		if (kern_path(pathname->name, LOOKUP_FOLLOW, &path_holder))
+			goto out;  /* Propogate the original err. */
+		victim_path = &path_holder;
+		mapping = d_backing_inode(victim_path->dentry)->i_mapping;
+		victim = NULL;
+	} else {
+		mapping = victim->f_mapping;
+	}
 	spin_lock(&swap_lock);
 	plist_for_each_entry(p, &swap_active_head, list) {
 		if (p->flags & SWP_WRITEOK) {
-			if (p->swap_file->f_mapping == mapping) {
+			struct dentry *dentry = p->swap_file->f_path.dentry;
+			if (!dentry)
+				continue; /* negative dentry */
+			if (dentry->d_inode->i_mapping == mapping) {
+				if (victim_path)
+					mapping = p->swap_file->f_mapping;
 				found = 1;
 				break;
 			}
@@ -2671,7 +2684,10 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 	wake_up_interruptible(&proc_poll_wait);
 
 out_dput:
-	filp_close(victim, NULL);
+	if (victim)
+		filp_close(victim, NULL);
+	if (victim_path)
+		path_put(victim_path);
 out:
 	putname(pathname);
 	return err;
@@ -2876,6 +2892,8 @@ static struct swap_info_struct *alloc_swap_info(void)
 
 static int claim_swapfile(struct swap_info_struct *p, struct inode *inode)
 {
+	bool disk_based_swap_enabled = IS_ENABLED(CONFIG_DISK_BASED_SWAP);
+
 	if (S_ISBLK(inode->i_mode)) {
 		p->bdev = I_BDEV(inode);
 		/*
@@ -2887,6 +2905,8 @@ static int claim_swapfile(struct swap_info_struct *p, struct inode *inode)
 			return -EINVAL;
 		p->flags |= SWP_BLKDEV;
 	} else if (S_ISREG(inode->i_mode)) {
+		if (!disk_based_swap_enabled)
+			return -EINVAL;
 		p->bdev = inode->i_sb->s_bdev;
 	}
 
