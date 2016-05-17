bd9817b32419d7e4be3480411dbb9f071ccf21b9
Kevin Cernekee <cernekee@chromium.org>
2016-05-16 22:28:46 -0700

    CHROMIUM: Introduce ANDROID_PARANOID_NETWORK as per-netns setting
    ANDROID_PARANOID_NETWORK requires creators of AF_INET/AF_INET6 sockets
    to be in the AID_INET group.  In our system it is problematic to enable
    this globally.  Change the logic so that the changes apply on a per-netns
    basis.  Also, interpret AID_* as gids instead of kgids.
    
    BUG=b:27932716
    TEST=`echo 1 > /proc/sys/net/core/android_paranoid`
    
    Signed-off-by: Kevin Cernekee <cernekee@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/362671
    Reviewed-by: Guenter Roeck <groeck@google.com>
    
    Conflicts:
    	Documentation/android.txt (deleted locally)
    	include/net/sock.h
    	net/core/sock.c
    	net/core/sysctl_net_core.c
    	net/ipv6/af_inet6.c
    	security/commoncap.c
    	net/ipv4/af_inet.c
    
    [rebase412(groeck): Clean version, not depending on Android
    	version of ANDROID_PARANOID_NETWORK]
    
    Conflicts:
    	net/core/sysctl_net_core.c
    	net/ipv4/af_inet.c
    	net/ipv6/af_inet6.c
    	security/commoncap.c
    
    [rebase419(groeck): Resolve various conflicts]
    Signed-off-by: Guenter Roeck <groeck@google.com>
    
    (cherry picked from commit eeff9e17e144063f761163315007ea67d42106ab)
    
    Conflicts:
    	drivers/net/tun.c
    	net/core/sysctl_net_core.c
    	net/ipv4/af_inet.c
    	net/ipv6/af_inet6.c
    	security/commoncap.c
    
    [Cherry-pick to 5.4: Resolve various conflicts]
    
    Signed-off-by: Taoyu Li <taoyl@google.com>
    Change-Id: Ie14fbfbe24d875bb103bcaebb3edd1e4528baf3b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2213509
    Tested-by: Taoyu Li <taoyl@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-by: Hugo Benichi <hugobenichi@google.com>
    Commit-Queue: Taoyu Li <taoyl@chromium.org>
    Conflicts: net/core/sock.c
    [rebase510(groeck): Context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    [rebase515(groeck):
      Squashed:
        FIXUP: CHROMIUM: Introduce ANDROID_PARANOID_NETWORK as per-netns setting
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Kcr-patch: 7427b21e8aa5f68ad42993d4e5918328c1bce25adbf15dfa4289fc6b.patch
    Change-Id: Ieb3b07d6fe6854684ec275424b63382170b4e8ba
    Kcr-patch: c1abe7ddfa94e2a009e6a557f68c266f117b25c169142bef52b00840.patch

--- net/core/sysctl_net_core.c
+++ net/core/sysctl_net_core.c
@@ -649,6 +649,15 @@ static struct ctl_table netns_core_table[] = {
 		.proc_handler	= proc_dointvec_minmax
 	},
 	{
+		.procname	= "android_paranoid",
+		.data		= &init_net.core.sysctl_android_paranoid,
+		.maxlen		= sizeof(int),
+		.mode		= 0644,
+		.extra1		= SYSCTL_ZERO,
+		.extra2		= SYSCTL_ONE,
+		.proc_handler	= proc_dointvec_minmax
+	},
+	{
 		.procname	= "optmem_max",
 		.data		= &init_net.core.sysctl_optmem_max,
 		.maxlen		= sizeof(int),
@@ -719,6 +728,8 @@ static __net_init int sysctl_core_net_init(struct net *net)
 {
 	size_t table_size = ARRAY_SIZE(netns_core_table);
 	struct ctl_table *tbl;
+
+	net->core.sysctl_android_paranoid = 0;
 
 	tbl = netns_core_table;
 	if (!net_eq(net, &init_net)) {
