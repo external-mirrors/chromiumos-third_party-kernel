13959af96dbc9d779b777c2b76b728b9e4521427
Bartosz Szczepanek <bsz@semihalf.com>
2020-06-12 18:06:18 +0200

    CHROMIUM: i2c-hid: retry SMBUS read sequence on failure
    This is a mitigation for problem specific to Rose touchpad on Eve. In
    case of warm boot, touchpad doesn't always respond correctly to SMBUS
    read sequence what causes i2c-hid driver to not attach even though
    device is on the bus.
    This temporary patch makes i2c-hid repeat the sequence 10 times before
    it returns.
    
    BUG=b:156232671
    TEST=Reboot Eve 10-20 times. Make sure touchpad is working.
    TOREVERT=Revert this as soon as Eve touchpad NACK issue is resolved.
    
    Change-Id: Id85c3c6121148f09192433792846f7b04fe69622
    Signed-off-by: Bartosz Szczepanek <bsz@semihalf.com>
    Signed-off-by: Radoslaw Biernacki <biernacki@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2243178
    Reviewed-by: Alex Levin <levinale@google.com>
    Commit-Queue: Alex Levin <levinale@google.com>
    Kcr-patch: e6efd1d689325b7cf0b97a38c15607b02c49597e4eadce67cb82ad72.patch

--- drivers/hid/i2c-hid/i2c-hid-core.c
+++ drivers/hid/i2c-hid/i2c-hid-core.c
@@ -67,6 +67,10 @@
 #define I2C_HID_PWR_ON		0x00
 #define I2C_HID_PWR_SLEEP	0x01
 
+#define EVE_TP_I2C_ADDR	0x49
+#define EVE_TP_RETRIES	10
+#define EVE_TP_DELAY_MS 1
+
 #define i2c_hid_dbg(ihid, ...) dev_dbg(&(ihid)->client->dev, __VA_ARGS__)
 
 struct i2c_hid_desc {
@@ -943,7 +947,7 @@ static void i2c_hid_core_shutdown_tail(struct i2c_hid *ihid)
 int i2c_hid_core_probe(struct i2c_client *client, struct i2chid_ops *ops,
 		       u16 hid_descriptor_address, u32 quirks)
 {
-	int ret;
+	int ret, retries = 0;
 	struct i2c_hid *ihid;
 	struct hid_device *hid;
 
@@ -990,8 +994,22 @@ int i2c_hid_core_probe(struct i2c_client *client, struct i2chid_ops *ops,
 
 	device_enable_async_suspend(&client->dev);
 
+	/*
+	 * Eve touchpad does not consistently ACK the slave address.
+	 * Retry the SMBUS sequence as a mitigation until proper fix
+	 * is prepared.
+	 * This is a temporary workaround for b/156232671.
+	 */
+	if (client->addr == EVE_TP_I2C_ADDR)
+		retries = EVE_TP_RETRIES;
+
 	/* Make sure there is something at this address */
 	ret = i2c_smbus_read_byte(client);
+	while ((ret < 0) && (retries--)) {
+		dev_err(&client->dev, "no response, retry left %d", retries);
+		msleep(EVE_TP_DELAY_MS);
+		ret = i2c_smbus_read_byte(client);
+	}
 	if (ret < 0) {
 		i2c_hid_dbg(ihid, "nothing at this address: %d\n", ret);
 		ret = -ENXIO;
