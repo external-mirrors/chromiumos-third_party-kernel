a35da95d103074fc82d8b5f2030c729f98d72833
Sean Paul <seanpaul@chromium.org>
2020-08-18 17:05:01 -0400

    CHROMIUM: drm/print: rename drm_debug* to be more syslog-centric
    In preparation for tracefs support, rename drm_debug related functions
    to reflect that it targets the syslog. This will allow us to selectively
    target syslog and/or tracefs.
    
    No functional changes here.
    
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20200608210505.48519-6-sean@poorly.run #v5
    
    Changes in v5:
    -Added to the set
    Changes in v6:
    -None
    (am from https://patchwork.freedesktop.org/patch/386270/)
    (also found at https://lore.kernel.org/r/20200818210510.49730-6-sean@poorly.run)
    
    Downstream reason:
    -Patchset needs more work to resolve dynamic debug conflicts and
     adoption upstream
    
    BUG=b:193917467
    TEST=Tested on volteer/zork/trogdor. Build tested on others
    
    Change-Id: I95ed43599ebce89ddf93faac0932673d207b14e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3035726
    Reviewed-by: Gil Dekel <gildekel@chromium.org>
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Sean Paul <seanpaul@chromium.org>
    Kcr-patch: f406a9fcf655ab28a44e146373cee8878bc667595ac4c46994ea604f.patch

--- include/drm/drm_print.h
+++ include/drm/drm_print.h
@@ -38,7 +38,7 @@
 struct drm_device;
 
 /* Do *not* use outside of drm_print.[ch]! */
-extern unsigned long __drm_debug;
+extern unsigned long __drm_debug_syslog;
 
 /**
  * DOC: print
@@ -140,9 +140,14 @@ enum drm_debug_category {
 	DRM_UT_DRMRES
 };
 
+static inline bool drm_debug_syslog_enabled(enum drm_debug_category category)
+{
+	return unlikely(__drm_debug_syslog & category);
+}
+
 static inline bool drm_debug_enabled_raw(enum drm_debug_category category)
 {
-	return unlikely(__drm_debug & BIT(category));
+	return drm_debug_syslog_enabled(category);
 }
 
 #define drm_debug_enabled_instrumented(category)			\
@@ -184,7 +189,7 @@ void __drm_puts_coredump(struct drm_printer *p, const char *str);
 void __drm_printfn_seq_file(struct drm_printer *p, struct va_format *vaf);
 void __drm_puts_seq_file(struct drm_printer *p, const char *str);
 void __drm_printfn_info(struct drm_printer *p, struct va_format *vaf);
-void __drm_printfn_dbg(struct drm_printer *p, struct va_format *vaf);
+void __drm_printfn_debug_syslog(struct drm_printer *p, struct va_format *vaf);
 void __drm_printfn_err(struct drm_printer *p, struct va_format *vaf);
 
 __printf(2, 3)
@@ -330,7 +335,7 @@ static inline struct drm_printer drm_dbg_printer(struct drm_device *drm,
 						 const char *prefix)
 {
 	struct drm_printer p = {
-		.printfn = __drm_printfn_dbg,
+		.printfn = __drm_printfn_debug_syslog,
 		.arg = drm,
 		.prefix = prefix,
 		.category = category,
