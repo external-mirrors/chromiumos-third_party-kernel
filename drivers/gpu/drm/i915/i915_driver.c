296f246c42c1d8168564a8e8b1236ac880e59cbc
Huang, Sean Z <sean.z.huang@intel.com>
2020-12-09 22:45:02 -0800

    CHROMIUM: drm/i915/pxp: Terminate the session upon app crash
    PXP should terminate the hardware session and cleanup the software
    state gracefully when the application has established the
    protection session, but doesn't close the session correctly due to
    some cases like application crash.
    
    Signed-off-by: Huang, Sean Z <sean.z.huang@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    Reviewed by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Signed-off-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    
    BUG=b:396348727
    TEST=i915 driver loaded successfully
    
    Change-Id: Ic11c3d49740fbdde6d300d55aa8928f4fbc0d9b8
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5231523
    Reviewed-by: Juston Li <justonli@google.com>
    Tested-by: Altaf Basha <altaf.basha@intel.com>
    Signed-off-by: Fnu Vishwanatha<fnu.vishwanatha@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6301106
    Commit-Queue: Marek Maślanka <mmaslanka@google.com>
    Tested-by: FNU VISHWANATHA <fnu.vishwanatha@intel.com>
    Reviewed-by: Kornel Dulęba <korneld@google.com>

--- drivers/gpu/drm/i915/i915_driver.c
+++ drivers/gpu/drm/i915/i915_driver.c
@@ -932,6 +932,7 @@ static void i915_driver_postclose(struct drm_device *dev, struct drm_file *file)
 
 	/* Catch up with all the deferred frees from "this" client */
 	i915_gem_flush_free_objects(to_i915(dev));
+	intel_pxp_close(to_i915(dev)->pxp, file);
 }
 
 void i915_driver_shutdown(struct drm_i915_private *i915)
