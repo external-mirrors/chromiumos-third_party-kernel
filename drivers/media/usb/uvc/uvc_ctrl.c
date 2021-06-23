b974726e58867a2b913a81e89cd7186a99c1df2d
Sergey Senozhatsky <senozhatsky@chromium.org>
2021-06-23 12:08:36 +0900

    FROMLIST: media: uvcvideo: add ROI auto controls
    This patch adds support for Region of Interest bmAutoControls.
    
    ROI control is a compound data type:
      Control Selector     CT_REGION_OF_INTEREST_CONTROL
      Mandatory Requests   SET_CUR, GET_CUR, GET_MIN, GET_MAX, GET_DEF
      wLength 10
      Offset   Field            Size
      0        wROI_Top         2
      2        wROI_Left        2
      4        wROI_Bottom      2
      6        wROI_Right       2
      8        bmAutoControls   2       (Bitmap)
    
    uvc_control_mapping, however, can handle only s32 data type at the
    moment: ->get() returns s32 value, ->set() accepts s32 value; while
    v4l2_ctrl maximum/minimum/default_value can hold only s64 values.
    
    Hence ROI control handling is split into two patches:
    a) bmAutoControls is handled via uvc_control_mapping as V4L2_CTRL_TYPE_BITMASK
    b) ROI rectangle (SET_CUR, GET_CUR, GET_DEF, etc.) handling is implemented
       separately, by the means of selection API.
    
    [senozhatsky: this patch includes an extra change that uses SHRT_MAX as
                  CID value, see crrev.com/c/2984868]
    
    (am from https://lore.kernel.org/lkml/20210501082001.100533-4-senozhatsky@chromium.org)
    
    BUG=b:186835892
    TEST=backport of a series that was tested on v4.19 kernel
    TEST=v4l2-ctl on hatch and drallion
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Change-Id: Ibe3194dca45bf6746b2475e0b5d9f0b9856c4846
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2981316
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>
    
    [rebase66(tzungbi):
     Revised commit message.
     Squashed:
       CHROMIUM: media: uvc: Remove static name definition of roi ctrl
         (https://crrev.com/c/3311280)
    ]
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    
    Kcr-patch: 08a0c84a0a4dcf9b8e830fe32053fdefae0e8121844064b5081692fb.patch
    Change-Id: Ib3dd920b5bfa921f5b65c6c210cd0afaa83ba142

--- drivers/media/usb/uvc/uvc_ctrl.c
+++ drivers/media/usb/uvc/uvc_ctrl.c
@@ -358,6 +358,15 @@ static const struct uvc_control_info uvc_ctrls[] = {
 		.flags		= UVC_CTRL_FLAG_GET_CUR
 				| UVC_CTRL_FLAG_AUTO_UPDATE,
 	},
+	{
+		.entity		= UVC_GUID_UVC_CAMERA,
+		.selector	= UVC_CT_REGION_OF_INTEREST_CONTROL,
+		.index		= 21,
+		.size		= 10,
+		.flags		= UVC_CTRL_FLAG_SET_CUR | UVC_CTRL_FLAG_GET_CUR
+				| UVC_CTRL_FLAG_GET_MIN | UVC_CTRL_FLAG_GET_MAX
+				| UVC_CTRL_FLAG_GET_DEF
+	},
 };
 
 static const u32 uvc_control_classes[] = {
@@ -840,6 +849,15 @@ static const struct uvc_control_mapping uvc_ctrl_mappings[] = {
 		.entity		= UVC_GUID_UVC_PROCESSING,
 		.selector	= UVC_PU_POWER_LINE_FREQUENCY_CONTROL,
 		.filter_mapping	= uvc_ctrl_filter_plf_mapping,
+	},
+	{
+		.id		= V4L2_CID_REGION_OF_INTEREST_AUTO,
+		.entity		= UVC_GUID_UVC_CAMERA,
+		.selector	= UVC_CT_REGION_OF_INTEREST_CONTROL,
+		.size		= 16,
+		.offset		= 64,
+		.v4l2_type	= V4L2_CTRL_TYPE_BITMASK,
+		.data_type	= UVC_CTRL_DATA_TYPE_BITMASK,
 	},
 };
 
