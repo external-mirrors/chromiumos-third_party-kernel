54920db720152db4cc0a22205f6f1e9fd8853f67
Brent Lu <brent.lu@intel.com>
2021-09-28 11:00:03 +0800

    CHROMIUM: ASoC: Intel: sof_amp_only: add adl_cs35l41 board config
    This machine driver is for boards which do not implement headphone
    codec. The board config adl_cs35l41 supports Cirrus Logic
    CS35L41/CS35L53 amplifier running on ADL platform.
    
    BUG=b:204343839, b:220262207
    TEST=ODM tests audio function on Vell
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Change-Id: I4f86cf96682f7a330fe4d413e6e5b50d3988f448
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3474413
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>
    (cherry picked from commit 8586ebb6358bbf9e92e42d50fdb6b0f911dc8ba0)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3676756
    Tested-by: Chien-Ming Huang <chien-ming.huang@quanta.corp-partner.google.com>
    Reviewed-by: Terry Cheong <htcheong@chromium.org>
    Commit-Queue: Terry Cheong <htcheong@chromium.org>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -384,6 +384,7 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.sof_fw_filename = "sof-adl.ri",
 		.sof_tplg_filename = "sof-adl-cs35l41.tplg",
 	},
+
 	{
 		.id = "10134242",
 		.drv_name = "adl_mx98360a_cs4242",
@@ -391,6 +392,8 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.quirk_data = &adl_max98360a_amp,
 		.sof_tplg_filename = "sof-adl-max98360a-rt5682.tplg",
 	},
+
+
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_adl_machines);
