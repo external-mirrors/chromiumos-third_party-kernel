d43853216143030c097cbc6097e614e730387444
Prashant Malani <pmalani@chromium.org>
2021-09-29 19:23:54 -0700

    BACKPORT: platform/chrome: cros_ec_proto: Add version for ec_command
    Add a version parameter to cros_ec_command() for callers that may want
    to specify which version of the host command they would like to use.
    
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Signed-off-by: Enric Balletbo i Serra <enric.balletbo@collabora.com>
    Link: https://lore.kernel.org/r/20210930022403.3358070-5-pmalani@chromium.org
    (cherry picked from commit 4f1406396ed4d97518b8112327bdaf14fc9d4090)
    
    BUG=b:287676025
    TEST=build kernel and verify HDMI CEC function
    
    Change-Id: I983245ffb72813fce2a10380f87ce713e4b3d869
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4852880
    Reviewed-by: Ricky Chang <rickytlchang@chromium.org>
    Reviewed-by: Reka Norman <rekanorman@chromium.org>
    Commit-Queue: Ricky Chang <rickytlchang@chromium.org>
    Tested-by: ChiJen Chen <chi-jen.chen@quanta.corp-partner.google.com>

--- include/linux/platform_data/cros_ec_proto.h
+++ include/linux/platform_data/cros_ec_proto.h
@@ -243,8 +243,8 @@ int cros_ec_get_sensor_count(struct cros_ec_dev *ec);
 
 bool cros_ec_handle_event(struct cros_ec_device *ec_dev);
 
-int cros_ec_command(struct cros_ec_device *ec_dev, int command, void *outdata, int outsize,
-		    void *indata, int insize);
+int cros_ec_command(struct cros_ec_device *ec_dev, unsigned int version, int command, void *outdata,
+		    int outsize, void *indata, int insize);
 
 /**
  * cros_ec_get_time_ns() - Return time in ns.
