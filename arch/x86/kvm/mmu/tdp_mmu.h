8f70b298e2a72560d11c603943d0ec3147c457ea
David Stevens <stevensd@chromium.org>
2021-10-15 12:30:21 -0400

    BACKPORT: KVM: cleanup allocation of rmaps and page tracking data
    Unify the flags for rmaps and page tracking data, using a
    single flag in struct kvm_arch and a single loop to go
    over all the address spaces and memslots.  This avoids
    code duplication between alloc_all_memslots_rmaps and
    kvm_page_track_enable_mmu_write_tracking.
    
    Signed-off-by: David Stevens <stevensd@chromium.org>
    [This patch is the delta between David's v2 and v3, with conflicts
     fixed and my own commit message. - Paolo]
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    
    (cherry picked from commit 1e76a3ce0d3cdfc6b506e21047a26471bc1cc92e)
    
    Conflicts:
       arch/x86/kvm/mmu/mmu.c
       arch/x86/kvm/mmu/page_track.c
    
    BUG=b:229579114
    TEST=boot brya-manatee, launch ARCVM on brya
    
    Change-Id: I56eea2debf29f9bda7de77616ca17846870c13f2
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3634597
    Reviewed-by: Junichi Uekawa <uekawa@chromium.org>
    Commit-Queue: David Stevens <stevensd@chromium.org>
    Tested-by: David Stevens <stevensd@chromium.org>

--- arch/x86/kvm/mmu/tdp_mmu.h
+++ arch/x86/kvm/mmu/tdp_mmu.h
@@ -76,12 +76,10 @@ int kvm_tdp_mmu_get_walk(struct kvm_vcpu *vcpu, u64 addr, u64 *sptes,
 #ifdef CONFIG_X86_64
 bool kvm_mmu_init_tdp_mmu(struct kvm *kvm);
 void kvm_mmu_uninit_tdp_mmu(struct kvm *kvm);
-static inline bool is_tdp_mmu_enabled(struct kvm *kvm) { return kvm->arch.tdp_mmu_enabled; }
 static inline bool is_tdp_mmu_page(struct kvm_mmu_page *sp) { return sp->tdp_mmu_page; }
 #else
 static inline bool kvm_mmu_init_tdp_mmu(struct kvm *kvm) { return false; }
 static inline void kvm_mmu_uninit_tdp_mmu(struct kvm *kvm) {}
-static inline bool is_tdp_mmu_enabled(struct kvm *kvm) { return false; }
 static inline bool is_tdp_mmu_page(struct kvm_mmu_page *sp) { return false; }
 #endif
 
