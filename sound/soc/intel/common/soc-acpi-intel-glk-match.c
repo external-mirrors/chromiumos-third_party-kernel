b85e9cf518bacb6c4a61d2fdbff687c599fa8f5a
Brent Lu <brent.lu@intel.com>
2021-10-28 22:09:09 +0800

    BACKPORT: ASoC: Intel: glk_rt5682_max98357a: support ALC5682I-VS codec
    Detect the codec variant in probe function and update DAI link
    accordingly. Also add an new entry in enumeration table for machine
    driver enumeration.
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Acked-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Link: https://lore.kernel.org/r/20211028140909.496022-1-brent.lu@intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit 88b4d77d6035dcf1182c4bf05c743e30363f3078)
    
    Conflicts:
       sound/soc/intel/boards/Kconfig
       sound/soc/intel/boards/glk_rt5682_max98357a.c
    
    BUG=b:198716740
    TEST=ODM tests audio function on Chrome device
    
    Change-Id: I37c1152a72905461acb0c91fd4e20f95be745015
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3234261
    Reviewed-by: Curtis Malainey <cujomalainey@chromium.org>
    Tested-by: Brent Lu <brent.lu@intel.com>
    Commit-Queue: Curtis Malainey <cujomalainey@chromium.org>
    (cherry picked from commit 285f853d126c4bc79d353cc688de155478927511)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3322505
    Reviewed-by: Asher Huang <asher_huang@pegatron.corp-partner.google.com>
    Tested-by: Asher Huang <asher_huang@pegatron.corp-partner.google.com>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>

--- sound/soc/intel/common/soc-acpi-intel-glk-match.c
+++ sound/soc/intel/common/soc-acpi-intel-glk-match.c
@@ -44,6 +44,15 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_glk_machines[] = {
 		.asoc_plat_name = "0000:00:0e.0",
 	},
 	{
+		.id = "RTL5682",
+		.drv_name = "glk_rt5682_max98357a",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &glk_codecs,
+		.sof_fw_filename = "intel/sof-glk.ri",
+		.sof_tplg_filename = "intel/sof-glk-rt5682.tplg",
+		.asoc_plat_name = "0000:00:0e.0",
+	},
+	{
 		.id = "10134242",
 		.drv_name = "glk_cs4242_max98357a",
 		.fw_filename = "intel/dsp_fw_glk.bin",
