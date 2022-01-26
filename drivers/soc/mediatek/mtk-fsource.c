37c8d7124aa4ca1fc966c6fc99953965ca751413
Lala Lin <lala.lin@mediatek.corp-partner.google.com>
2022-01-26 15:58:42 +0800

    CHROMIUM: fsource: add fsource driver to enable vefuse
    add fsource driver to enable vefuse
    
    BUG=b:185745886
    
    TEST=build pass and boot to shell
    echo 1 > /sys/bus/platform/drivers/mtk-fsource/fsource/state
    echo 0 > /sys/bus/platform/drivers/mtk-fsource/fsource/state
    
    Change-Id: I50f8b657998fabdbc5cb1a36a70d76229a18bac1
    Signed-off-by: Lala Lin <lala.lin@mediatek.corp-partner.google.com>
    (cherry picked from commit I17a9ab73ee8a0b8127658aa686d9284e7aaf4264)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3419032
    Reviewed-by: Yu-An Wang <wyuang@google.com>
    Commit-Queue: Yu-An Wang <wyuang@google.com>
    Tested-by: Yu-An Wang <wyuang@google.com>

---
+++ drivers/soc/mediatek/mtk-fsource.c
@@ -0,0 +1,159 @@
+// SPDX-License-Identifier: GPL-2.0
+//
+// Copyright (c) 2019 MediaTek Inc.
+
+#include <linux/err.h>
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/of_device.h>
+#include <linux/platform_device.h>
+#include <linux/pm_domain.h>
+#include <linux/regulator/consumer.h>
+#include <linux/slab.h>
+
+static struct regulator *buck;
+
+static const struct of_device_id fsource_of_match[] = {
+	{ .compatible = "mtk-fsource" },
+	{ }
+};
+
+MODULE_DEVICE_TABLE(of, fsource_of_match);
+
+static ssize_t reg_show_state(struct device *dev,
+			  struct device_attribute *attr, char *buf)
+{
+	if (!buck)
+		return sprintf(buf, "not exist\n");
+
+	if (regulator_is_enabled(buck))
+		return sprintf(buf, "enabled\n");
+
+	return sprintf(buf, "disabled\n");
+}
+
+static ssize_t reg_set_state(struct device *dev, struct device_attribute *attr,
+			 const char *buf, size_t count)
+{
+	bool enabled;
+	int ret;
+
+	if (!buck) {
+		dev_err(dev, "not exist\n");
+		return count;
+	}
+
+	/*
+	 * sysfs_streq() doesn't need the \n's, but we add them so the strings
+	 * will be shared with show_state(), above.
+	 */
+	if (sysfs_streq(buf, "enabled\n") || sysfs_streq(buf, "1"))
+		enabled = true;
+	else if (sysfs_streq(buf, "disabled\n") || sysfs_streq(buf, "0"))
+		enabled = false;
+	else {
+		dev_err(dev, "Configuring invalid mode\n");
+		return count;
+	}
+
+	if (enabled != regulator_is_enabled(buck)) {
+		if (enabled) {
+			ret = regulator_enable(buck);
+		} else {
+			ret = regulator_disable(buck);
+		}
+
+		if (ret != 0)
+			dev_err(dev, "Failed to configure state: %d\n", ret);
+	}
+
+	return count;
+}
+static DEVICE_ATTR(state, 0644, reg_show_state, reg_set_state);
+
+static struct attribute *attributes[] = {
+	&dev_attr_state.attr,
+	NULL,
+};
+
+static const struct attribute_group attr_group = {
+	.attrs	= attributes,
+};
+
+static int fsource_probe(struct platform_device *pdev)
+{
+	int ret = -EINVAL;
+
+	pr_notice("[%s]\n", __func__);
+
+	buck = regulator_get(&pdev->dev, "vfsource");
+	if (IS_ERR(buck)) {
+		pr_notice("%s: cannot get regulator \"vfsource-supply\"\n", __func__);
+		return IS_ERR(buck);
+	}
+
+	ret = sysfs_create_group(&pdev->dev.kobj, &attr_group);
+	if (ret != 0)
+		return ret;
+
+	return 0;
+}
+
+static int fsource_remove(struct platform_device *pdev)
+{
+	int ret = -EINVAL;
+
+	sysfs_remove_group(&pdev->dev.kobj, &attr_group);
+
+	ret = regulator_disable(buck);
+	if (ret)
+		pr_notice("%s: fail to disable vfsource power: %d\n", __func__, ret);
+
+	return 0;
+}
+
+static void fsource_shutdown(struct platform_device *pdev)
+{
+	int ret = -EINVAL;
+
+	sysfs_remove_group(&pdev->dev.kobj, &attr_group);
+
+	ret = regulator_disable(buck);
+	if (ret)
+		pr_notice("%s: fail to disable vfsource power: %d\n", __func__, ret);
+}
+
+static struct platform_driver fsource_driver = {
+	.driver		= {
+		.name		= "mtk-fsource",
+		.of_match_table	= of_match_ptr(fsource_of_match),
+	},
+	.probe		= fsource_probe,
+	.remove		= fsource_remove,
+	.shutdown	= fsource_shutdown,
+};
+
+static int __init fsource_init(void)
+{
+	int ret;
+
+	ret = platform_driver_register(&fsource_driver);
+	if (ret) {
+		pr_err("[%s] platform driver register failed: %d\n", __func__, ret);
+		return ret;
+	}
+
+	return 0;
+}
+late_initcall(fsource_init);
+
+static void __exit fsource_exit(void)
+{
+    platform_driver_unregister(&fsource_driver);
+}
+module_exit(fsource_exit);
+
+MODULE_AUTHOR("Hsin-Hsiung Wang, MediaTek");
+MODULE_DESCRIPTION("MediaTek Fsource Driver");
+MODULE_LICENSE("GPL v2");
