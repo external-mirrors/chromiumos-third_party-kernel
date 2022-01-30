16f080e7c0427587456c0cb94ce7a34514b2a65e
Łukasz Bartosik <ukaszb@google.com>
2022-01-30 14:42:11 +0100

    FIXUP: CHROMIUM: rtw88: replace RADAR flag with NO_IR for dfs channel
    Create fixup.
    
    Signed-off-by: Łukasz Bartosik <ukaszb@google.com>
    Change-Id: I86dd5367380289cce33d07fdeaaaa203c0fd57d9

--- drivers/net/wireless/realtek/rtw88/main.h
+++ drivers/net/wireless/realtek/rtw88/main.h
@@ -2131,5 +2131,7 @@ void rtw_core_fw_scan_notify(struct rtw_dev *rtwdev, bool start);
 int rtw_dump_fw(struct rtw_dev *rtwdev, const u32 ocp_src, u32 size,
 		u32 fwcd_item);
 int rtw_dump_reg(struct rtw_dev *rtwdev, const u32 addr, const u32 size);
+void rtw_restore_no_ir_flag(struct rtw_dev *rtwdev);
+void rtw_replace_radar_flag_with_no_ir(struct ieee80211_hw *hw);
 
 #endif
