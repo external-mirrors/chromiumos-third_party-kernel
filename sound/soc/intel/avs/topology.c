d6c85e6b143325b6aec8c4f5875cba0df76ca6f4
Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
2022-02-21 13:57:19 +0100

    CHROMIUM: ASoC: Intel: avs: Make kcontrols API more generic
    In order to add support for TLV kcontrols make already present volume
    kcontrol API more generic, by separating data part from kcontrol
    private_data.
    
    BUG=b:219931843
    TEST=AVS works on APL and AML
    
    Change-Id: Ibfa7eaba132d25fc07fe4c672a8065d6003bc581
    Signed-off-by: Lukasz Majczak <lmajczak@google.com>
    Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>

--- sound/soc/intel/avs/topology.c
+++ sound/soc/intel/avs/topology.c
@@ -1526,9 +1526,8 @@ static int avs_widget_load(struct snd_soc_component *comp, int index,
 	return 0;
 }
 
-static int avs_widget_ready(struct snd_soc_component *comp, int index,
-			    struct snd_soc_dapm_widget *w,
-			    struct snd_soc_tplg_dapm_widget *dw)
+static int avs_register_volume_kcontrols(struct snd_soc_component *comp,
+					 struct snd_soc_dapm_widget *w)
 {
 	struct avs_tplg_path_template *template = w->priv;
 	struct avs_tplg_pipeline *pipeline;
@@ -1538,7 +1537,11 @@ static int avs_widget_ready(struct snd_soc_component *comp, int index,
 	int i, k, max = 0;
 	int *chns; /* maximum channels per kcontrol */
 
-	/* Find maximum number of peakvols on path. */
+	/*
+	 * Count PEAKVOLs on each path template before creating kcontrols for
+	 * path. This is needed to index them if there is more than one
+	 * PEAKVOL present in any of path templates.
+	 */
 	list_for_each_entry(path, &template->path_list, node) {
 		i = 0;
 
@@ -1556,11 +1559,16 @@ static int avs_widget_ready(struct snd_soc_component *comp, int index,
 			max = i;
 	}
 
-	/* No kcontrols, don't loop unnecessarily */
+	/* No PEAKVOLs found, so leave early */
 	if (!max)
 		return 0;
 
-	/* Find maximum number of channels we will need on each kcontrol. */
+	/*
+	 * Now that PEAKVOLs are counted, find number of channels needed.
+	 * We walk all path templates again, as for example one path
+	 * template can have PEAKVOL in 2 channel configuration, while
+	 * another in 4 channel.
+	 */
 	chns = kcalloc(max, sizeof(*chns), GFP_KERNEL);
 	if (!chns)
 		return -ENOMEM;
@@ -1582,7 +1590,10 @@ static int avs_widget_ready(struct snd_soc_component *comp, int index,
 		}
 	}
 
-	/* Create and assign kcontrol to tplg module. */
+	/*
+	 * For each PAEKVOL we found, create and assign kcontrol to tplg module
+	 * template.
+	 */
 	for (k = 0; k < max; k++) {
 		struct snd_kcontrol *kctrl;
 
@@ -1613,6 +1624,19 @@ static int avs_widget_ready(struct snd_soc_component *comp, int index,
 	kfree(chns);
 
 	return 0;
+}
+
+static int avs_widget_ready(struct snd_soc_component *comp, int index,
+			    struct snd_soc_dapm_widget *w,
+			    struct snd_soc_tplg_dapm_widget *dw)
+{
+	int ret;
+
+	ret = avs_register_volume_kcontrols(comp, w);
+	if (ret < 0)
+		dev_err(comp->dev, "failed to register volume kcontrol: %d\n", ret);
+
+	return ret;
 }
 
 static int avs_dai_load(struct snd_soc_component *comp, int index,
