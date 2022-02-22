e0026ca38cf16b3b61f52601e18e09fdd6f6c9f6
Luigi Santivetti <luigi.santivetti@imagination.corp-partner.google.com>
2022-02-22 18:18:51 +0000

    CHROMIUM: img-rogue/1.13: fix write OOB in MMU_MapPages heap
    Always check PageCount and PhyPgOffset
    
    BUG=b:217211551 b:224685048
    TEST=none
    
    Signed-off-by: Luigi Santivetti <luigi.santivetti@imagination.corp-partner.google.com>
    Change-Id: Id1511c8832a88fdf4da0e15249c4719403791df4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3560793
    Reviewed-by: Martin Faltesek <mfaltesek@google.com>
    Tested-by: Eizan Miyamoto <eizan@chromium.org>
    Commit-Queue: Eizan Miyamoto <eizan@chromium.org>

--- drivers/gpu/drm/img-rogue/1.13/devicemem_server.c
+++ drivers/gpu/drm/img-rogue/1.13/devicemem_server.c
@@ -595,6 +595,9 @@ DevmemIntMapPages(DEVMEMINT_RESERVATION *psReservation,
 {
 	PVRSRV_ERROR eError;
 
+	PVR_LOG_RETURN_IF_INVALID_PARAM((ui32PageCount < PMR_MAX_SUPPORTED_PAGE_COUNT), "ui32PageCount");
+	PVR_LOG_RETURN_IF_INVALID_PARAM((ui32PhysicalPgOffset < PMR_MAX_SUPPORTED_PAGE_COUNT), "ui32PhysicalPgOffset");
+
 	if (psReservation->psDevmemHeap->uiLog2PageSize > PMR_GetLog2Contiguity(psPMR))
 	{
 		PVR_DPF((PVR_DBG_ERROR,
@@ -624,6 +627,8 @@ DevmemIntUnmapPages(DEVMEMINT_RESERVATION *psReservation,
                     IMG_DEV_VIRTADDR sDevVAddrBase,
                     IMG_UINT32 ui32PageCount)
 {
+	PVR_LOG_RETURN_IF_INVALID_PARAM((ui32PageCount < PMR_MAX_SUPPORTED_PAGE_COUNT), "ui32PageCount");
+
 	/* Unmap the pages and mark them invalid in the MMU PTE */
 	MMU_UnmapPages(psReservation->psDevmemHeap->psDevmemCtx->psMMUContext,
 	               0,
