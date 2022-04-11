fe064b46a401699c796f5691f68145d9b41d277e
Łukasz Bartosik <ukaszb@google.com>
2022-04-11 10:39:17 +0200

    FIXUP: BACKPORT: FROMLIST: kvm/x86: virtual suspend time injection: Implement host side
    Create fixup.
    
    Signed-off-by: Łukasz Bartosik <ukaszb@google.com>
    Change-Id: Ib112a90eb97fe22a6ae80c8e67f36a5aeb648edf

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -931,7 +931,7 @@ static int kvm_init_mmu_notifier(struct kvm *kvm)
 static int kvm_suspend_notifier(struct kvm *kvm)
 {
 	struct kvm_vcpu *vcpu;
-	int i;
+	unsigned long i;
 
 	if (!virt_suspend_time_enabled(kvm))
 		return NOTIFY_DONE;
@@ -947,7 +947,7 @@ static int kvm_suspend_notifier(struct kvm *kvm)
 static int kvm_resume_notifier(struct kvm *kvm)
 {
 	struct kvm_vcpu *vcpu;
-	int i;
+	unsigned long i;
 
 	if (!virt_suspend_time_enabled(kvm))
 		return NOTIFY_DONE;
