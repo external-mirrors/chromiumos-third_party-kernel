682aa24e4d5f267950152b6228f2bee6d1027cff
Terry Chen <terry_chen@wistron.corp-partner.google.com>
2022-05-11 15:55:22 +0800

    Reland "BACKPORT: FROMLIST: ASoC: Intel: sof_cs42l42: adding support for ADL configuration and BT offload audio"
    This is a reland of commit 2393828dfd114e3792b302b93005f0ffa90d616e
    
    Original change's description:
    > BACKPORT: FROMLIST: ASoC: Intel: sof_cs42l42: adding support for ADL configuration and BT offload audio
    >
    > To be able to do  driver data for adl_mx98360a_cs4242 which supports
    > two max98360a speaker amplifiers on SSP1 and cs42l42 headphone codec
    > on SSP0 running on ADL platform. Also add the capability to machine driver
    > of creating DAI Link for BT offload. Although BT offload always uses SSP2
    > port but we reserve the flexibility to assign the port number in macro.
    >
    > Signed-off-by: Terry Chen <terry_chen@wistron.corp-partner.google.com>
    > (am from https://patchwork.kernel.org/patch/12845884/)
    > (also found at https://lore.kernel.org/r/20220511075522.1764114-1-terry_chen@wistron.corp-partner.google.com)
    >
    > BUG=b:230418589
    > TEST=emerge-brya chromeos-kernel-5_10
    >
    > Change-Id: I6b77b874bccb023ddd47d2e8c169c05dec2dda3a
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3638251
    > Tested-by: Terry Chen <terry_chen@wistron.corp-partner.google.com>
    > Commit-Queue: Terry Cheong <htcheong@chromium.org>
    > Reviewed-by: Terry Cheong <htcheong@chromium.org>
    > (cherry picked from commit 704b31b4a823abe75dc01423b89fcdb318cc769f)
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3655719
    > Commit-Queue: YH Lin <yueherngl@chromium.org>
    
    Bug: b:230418589
    Change-Id: I0c6f000c8feb2dd189dba544a455faf7cc53bb1f
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3656995
    Tested-by: Terry Chen <terry_chen@wistron.corp-partner.google.com>
    Reviewed-by: Cheng Yueh <cyueh@chromium.org>
    Commit-Queue: Cheng Yueh <cyueh@chromium.org>

--- sound/soc/intel/common/soc-acpi-intel-adl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-adl-match.c
@@ -371,6 +371,26 @@ struct snd_soc_acpi_mach snd_soc_acpi_intel_adl_machines[] = {
 		.sof_fw_filename = "sof-adl.ri",
 		.sof_tplg_filename = "sof-adl-nau8825.tplg",
 	},
+	{
+		.comp_ids = &adl_rt5682_rt5682s_hp,
+		.drv_name = "adl_rt5682",
+		.sof_fw_filename = "sof-adl.ri",
+		.sof_tplg_filename = "sof-adl-rt5682.tplg",
+	},
+	/* place amp-only boards in the end of table */
+	{
+		.id = "CSC3541",
+		.drv_name = "adl_cs35l41",
+		.sof_fw_filename = "sof-adl.ri",
+		.sof_tplg_filename = "sof-adl-cs35l41.tplg",
+	},
+	{
+		.id = "10134242",
+		.drv_name = "adl_mx98360a_cs4242",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &adl_max98360a_amp,
+		.sof_tplg_filename = "sof-adl-max98360a-rt5682.tplg",
+	},
 	{},
 };
 EXPORT_SYMBOL_GPL(snd_soc_acpi_intel_adl_machines);
