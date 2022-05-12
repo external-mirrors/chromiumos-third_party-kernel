c08e7209d66b1a73a75972125f61480ea1b00cc1
Phil Edworthy <phil.edworthy@renesas.com>
2022-05-12 12:47:19 +0100

    ravb: Separate handling of irq enable/disable regs into feature
    [ Upstream commit cb99badde146c327f150773921ffe080abe1eb44 ]
    
    Currently, when the HW has a single interrupt, the driver uses the
    GIC, TIC, RIC0 registers to enable and disable interrupts.
    When the HW has multiple interrupts, it uses the GIE, GID, TIE, TID,
    RIE0, RID0 registers.
    
    However, other devices, e.g. RZ/V2M, have multiple irqs and only have
    the GIC, TIC, RIC0 registers.
    Therefore, split this into a separate feature.
    
    Signed-off-by: Phil Edworthy <phil.edworthy@renesas.com>
    Reviewed-by: Biju Das <biju.das.jz@bp.renesas.com>
    Reviewed-by: Sergey Shtylyov <s.shtylyov@omp.ru>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Stable-dep-of: eac16a733427 ("net: ravb: Stop DMA in case of failures on ravb_open()")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- drivers/net/ethernet/renesas/ravb.h
+++ drivers/net/ethernet/renesas/ravb.h
@@ -1000,6 +1000,7 @@ struct ravb_hw_info {
 	unsigned internal_delay:1;	/* AVB-DMAC has internal delays */
 	unsigned tx_counters:1;		/* E-MAC has TX counters */
 	unsigned multi_irqs:1;		/* AVB-DMAC and E-MAC has multiple irqs */
+	unsigned irq_en_dis:1;		/* Has separate irq enable and disable regs */
 	unsigned gptp:1;		/* AVB-DMAC has gPTP support */
 	unsigned ccc_gac:1;		/* AVB-DMAC has gPTP support active in config mode */
 };
