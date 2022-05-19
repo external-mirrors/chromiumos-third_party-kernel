7849e929f7910029bbf4dc0ce0541fb681b810ae
Juston Li <juston.li@intel.com>
2022-05-19 09:34:38 -0700

    BACKPORT: FROMGIT: CHROMIUM: drm/i915/pxp: enable userspace to pass vtags
    Allow userspace to pass in the vtag associated with the session context.
    vtag is stored in the first byte of the output buffer passed in.
    
    cherry-pick(6.12) from crrev/c/3649896, originally from crrev/c/3649895.
    Conflicts:
      drivers/gpu/drm/i915/pxp/intel_pxp_tee.c
        removed unneeded vtag operations due to refactoring since 5.10.
    
    (cherry picked from commit ebe74f85ad67
    ("CHROMIUM: drm/i915/pxp: enable userspace to pass vtags")
    https://chromium.googlesource.com/chromiumos/third_party/kernel
    chromeos-5.10)
    
    BUG=b:493774090
    TEST=Played https://partner.widevine.com/player with HW_SECURE_ALL
    TEST=with --allow-ra-in-dev-mode in /etc/chrome_dev.conf
    TEST=and video playback worked without any issues.
    
    Change-Id: Iab50b6cc364ef9f4633a98954558331e54e01725
    Signed-off-by: Juston Li <juston.li@intel.com>
    Signed-off-by: Sudarshan S <sudarshan.s@intel.com>
    Signed-off-by: sachin kumar <sachin.kumar@intel.com>
    Signed-off-by: hikalium <hikalium@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7725422
    Reviewed-by: Nathan Hebert <nhebert@chromium.org>

--- drivers/gpu/drm/i915/pxp/intel_pxp_tee.c
+++ drivers/gpu/drm/i915/pxp/intel_pxp_tee.c
@@ -64,6 +64,7 @@ int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 	struct i915_pxp_component *pxp_component = pxp->pxp_component;
 	u8 tmp_drop_buf[64];
 	int ret = 0;
+	u8 vtag = 1;
 
 	mutex_lock(&pxp->tee_mutex);
 
@@ -76,6 +77,9 @@ int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 		goto unlock;
 	}
 
+	/* The vtag is stored in the first byte of the output buffer */
+	memcpy(&vtag, msg_out, sizeof(vtag));
+
 	if (pxp->mei_pxp_last_msg_interrupted) {
 		/* read and drop data from the previous iteration */
 		ret = pxp_component->ops->recv(pxp_component->tee_dev, &tmp_drop_buf, 64,
@@ -87,7 +91,7 @@ int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 	}
 
 	ret = pxp_component->ops->send(pxp_component->tee_dev, msg_in, msg_in_size,
-				       1, PXP_TRANSPORT_TIMEOUT_MS);
+				       vtag, PXP_TRANSPORT_TIMEOUT_MS);
 	if (ret) {
 		/* flag on next msg to drop interrupted msg */
 		if (ret == -EINTR)
@@ -97,7 +101,7 @@ int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 	}
 
 	ret = pxp_component->ops->recv(pxp_component->tee_dev, msg_out, msg_out_max_size,
-				       1, PXP_TRANSPORT_TIMEOUT_MS);
+				       vtag, PXP_TRANSPORT_TIMEOUT_MS);
 	if (ret < 0) {
 		/* flag on next msg to drop interrupted msg */
 		if (ret == -EINTR)
