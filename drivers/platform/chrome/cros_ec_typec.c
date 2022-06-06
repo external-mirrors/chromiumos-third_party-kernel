7c57bde73c949bfd8474577c997f333fc9a96512
Prashant Malani <pmalani@chromium.org>
2022-06-06 20:18:03 +0000

    BACKPORT: platform/chrome: cros_ec_proto: Rename cros_ec_command function
    cros_ec_command() is the name of a function as well as a struct, as such
    it can confuse indexing tools (like ctags). Avoid this by renaming it to
    cros_ec_cmd(). Update all the callsites to use the new name.
    
    This patch is a find-and-replace, so should not introduce any functional
    changes.
    
    Suggested-by: Stephen Boyd <swboyd@chromium.org>
    Signed-off-by: Prashant Malani <pmalani@chromium.org>
    Acked-by: Lee Jones <lee.jones@linaro.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Signed-off-by: Tzung-Bi Shih <tzungbi@kernel.org>
    Link: https://lore.kernel.org/r/20220606201825.763788-3-pmalani@chromium.org
    (cherry picked from commit b1d288d9c3c5ca28df062214656a59cf7ee370e0)
    
    Conflicts:
       drivers/mfd/cros_ec_dev.c
       drivers/platform/chrome/cros_ec_proto.c
       drivers/platform/chrome/cros_ec_typec.c
    
    BUG=b:270507438
    TEST=build
    
    Change-Id: I7ceee46c281c718dc2fb95c710f141d78359d531
    Signed-off-by: Reka Norman <rekanorman@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4659339
    Tested-by: Reka Norman <rekanorman@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Reka Norman <rekanorman@chromium.org>
    (cherry picked from commit 73a5054d18b9318a230f9662cd4d028d3575cae4)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4857973
    Reviewed-by: Reka Norman <rekanorman@chromium.org>
    Commit-Queue: Ricky Chang <rickytlchang@chromium.org>
    Tested-by: ChiJen Chen <chi-jen.chen@quanta.corp-partner.google.com>
    Reviewed-by: Ricky Chang <rickytlchang@chromium.org>

--- drivers/platform/chrome/cros_ec_typec.c
+++ drivers/platform/chrome/cros_ec_typec.c
@@ -518,8 +518,8 @@ static int cros_typec_configure_mux(struct cros_typec_data *typec, int port_num,
 	/* Sending Acknowledgment to EC */
 	mux_ack.port = port_num;
 
-	if (cros_ec_command(typec->ec, 0, EC_CMD_USB_PD_MUX_ACK, &mux_ack,
-			    sizeof(mux_ack), NULL, 0) < 0)
+	if (cros_ec_cmd(typec->ec, 0, EC_CMD_USB_PD_MUX_ACK, &mux_ack,
+			sizeof(mux_ack), NULL, 0) < 0)
 		dev_warn(typec->dev,
 			 "Failed to send Mux ACK to EC for port: %d\n",
 			 port_num);
@@ -591,8 +591,8 @@ static int cros_typec_get_mux_info(struct cros_typec_data *typec, int port_num,
 		.port = port_num,
 	};
 
-	return cros_ec_command(typec->ec, 0, EC_CMD_USB_PD_MUX_INFO, &req,
-			       sizeof(req), resp, sizeof(*resp));
+	return cros_ec_cmd(typec->ec, 0, EC_CMD_USB_PD_MUX_INFO, &req,
+			   sizeof(req), resp, sizeof(*resp));
 }
 
 static int cros_typec_register_altmodes(struct cros_typec_data *typec, int port_num)
@@ -658,8 +658,8 @@ static int cros_typec_handle_sop_disc(struct cros_typec_data *typec, int port_nu
 	}
 
 	memset(sop_disc, 0, EC_PROTO2_MAX_RESPONSE_SIZE);
-	ret = cros_ec_command(typec->ec, 0, EC_CMD_TYPEC_DISCOVERY, &req, sizeof(req),
-			      sop_disc, EC_PROTO2_MAX_RESPONSE_SIZE);
+	ret = cros_ec_cmd(typec->ec, 0, EC_CMD_TYPEC_DISCOVERY, &req, sizeof(req),
+			  sop_disc, EC_PROTO2_MAX_RESPONSE_SIZE);
 	if (ret < 0) {
 		dev_err(typec->dev, "Failed to get SOP discovery data for port: %d\n", port_num);
 		goto disc_exit;
@@ -701,8 +701,8 @@ static void cros_typec_handle_status(struct cros_typec_data *typec, int port_num
 	};
 	int ret;
 
-	ret = cros_ec_command(typec->ec, 0, EC_CMD_TYPEC_STATUS, &req, sizeof(req),
-			      &resp, sizeof(resp));
+	ret = cros_ec_cmd(typec->ec, 0, EC_CMD_TYPEC_STATUS, &req, sizeof(req),
+			  &resp, sizeof(resp));
 	if (ret < 0) {
 		dev_warn(typec->dev, "EC_CMD_TYPEC_STATUS failed for port: %d\n", port_num);
 		return;
@@ -741,9 +741,9 @@ static int cros_typec_port_update(struct cros_typec_data *typec, int port_num)
 	req.mux = USB_PD_CTRL_MUX_NO_CHANGE;
 	req.swap = USB_PD_CTRL_SWAP_NONE;
 
-	ret = cros_ec_command(typec->ec, typec->pd_ctrl_ver,
-			      EC_CMD_USB_PD_CONTROL, &req, sizeof(req),
-			      &resp, sizeof(resp));
+	ret = cros_ec_cmd(typec->ec, typec->pd_ctrl_ver,
+			  EC_CMD_USB_PD_CONTROL, &req, sizeof(req),
+			  &resp, sizeof(resp));
 	if (ret < 0)
 		return ret;
 
@@ -791,9 +791,8 @@ static int cros_typec_get_cmd_version(struct cros_typec_data *typec)
 
 	/* We're interested in the PD control command version. */
 	req_v1.cmd = EC_CMD_USB_PD_CONTROL;
-	ret = cros_ec_command(typec->ec, 1, EC_CMD_GET_CMD_VERSIONS,
-			      &req_v1, sizeof(req_v1), &resp,
-				    sizeof(resp));
+	ret = cros_ec_cmd(typec->ec, 1, EC_CMD_GET_CMD_VERSIONS,
+			  &req_v1, sizeof(req_v1), &resp, sizeof(resp));
 	if (ret < 0)
 		return ret;
 
@@ -816,8 +815,8 @@ static int cros_typec_feature_supported(struct cros_typec_data *typec, enum ec_f
 	struct ec_response_get_features resp = {};
 	int ret;
 
-	ret = cros_ec_command(typec->ec, 0, EC_CMD_GET_FEATURES, NULL, 0,
-			      &resp, sizeof(resp));
+	ret = cros_ec_cmd(typec->ec, 0, EC_CMD_GET_FEATURES, NULL, 0,
+			  &resp, sizeof(resp));
 	if (ret < 0) {
 		dev_warn(typec->dev,
 			 "Failed to get features, assuming typec feature=%d unsupported.\n",
@@ -892,8 +891,8 @@ static int cros_typec_probe(struct platform_device *pdev)
 	typec->needs_mux_ack = !!cros_typec_feature_supported(typec,
 					EC_FEATURE_TYPEC_MUX_REQUIRE_AP_ACK);
 
-	ret = cros_ec_command(typec->ec, 0, EC_CMD_USB_PD_PORTS, NULL, 0,
-			      &resp, sizeof(resp));
+	ret = cros_ec_cmd(typec->ec, 0, EC_CMD_USB_PD_PORTS, NULL, 0,
+			  &resp, sizeof(resp));
 	if (ret < 0)
 		return ret;
 
