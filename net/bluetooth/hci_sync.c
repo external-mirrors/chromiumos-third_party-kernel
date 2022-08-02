58fac30f533281e1903a2ff8df59641610499978
Manish Mandlik <mmandlik@google.com>
2022-08-02 15:21:37 -0700

    FROMLIST: Bluetooth: hci_sync: Cancel AdvMonitor interleave scan when suspend
    Cancel interleaved scanning for advertisement monitor when suspend.
    
    Fixes: 182ee45da083 ("Bluetooth: hci_sync: Rework hci_suspend_notifier")
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    (am from https://patchwork.kernel.org/patch/12935876)
    
    BUG=b:235985815
    TEST=Run advmon_interleaved_scan_tests
    
    Signed-off-by: Manish Mandlik <mmandlik@google.com>
    Change-Id: I27d3502a0851c75c0c31fb7fea9b09644d54d81d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3806144
    Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Manish Mandlik <mmandlik@chromium.org>
    Tested-by: Manish Mandlik <mmandlik@chromium.org>

--- net/bluetooth/hci_sync.c
+++ net/bluetooth/hci_sync.c
@@ -2064,6 +2064,9 @@ static bool is_interleave_scanning(struct hci_dev *hdev)
 
 static void cancel_interleave_scan(struct hci_dev *hdev)
 {
+	if (!is_interleave_scanning(hdev))
+		return;
+
 	bt_dev_dbg(hdev, "cancelling interleave scan");
 
 	cancel_delayed_work_sync(&hdev->interleave_scan);
@@ -5854,6 +5857,9 @@ int hci_suspend_sync(struct hci_dev *hdev)
 
 	/* Pause other advertisements */
 	hci_pause_advertising_sync(hdev);
+
+	/* Cancel interleaved scan */
+	cancel_interleave_scan(hdev);
 
 	/* Suspend monitor filters */
 	hci_suspend_monitor_sync(hdev);
