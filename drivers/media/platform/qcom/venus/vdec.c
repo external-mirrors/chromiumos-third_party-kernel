d271bebbcd615eee73fdc65e0f756a0f009786e6
Nathan Hebert <nhebert@chromium.org>
2022-08-15 11:40:45 -0700

    CHROMIUM: Revert "media: venus: vdec: Fix decoder cmd STOP issue"
    This reverts commit 83af5738dc13b407d653d6b6fa725d459fa2ab46.
    
    For HFI v6, Qualcomm supports two SOC's. RB5 and Herobrine. The end of
    stream (EOS) handling for the different SOC firmware are incompatible.
    The commit that this reverts fixed EOS handling for RB5, but broke
    handling for Herobrine.
    
    Qualcomm is updating the RB5 firmware so they can revert this commit
    upstream. Google does not use RB5 in any product, so reverting the
    commit is safe and will give us better tast data now.
    
    Signed-off-by: Nathan Hebert <nhebert@chromium.org>
    
    BUG=b:234021772
    TEST=tast run <DUT> video.PlatformDecoding.*
    
    Change-Id: Ia476100d0212115479af6c3033c7a8db41492910
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3832576
    Tested-by: Nathan Hebert <nhebert@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Commit-Queue: Nathan Hebert <nhebert@chromium.org>
    Kcr-patch: 3d2b1ab7e14b73ab1aff0fa609f95593206ef358b0b69ac1f2d46845.patch

--- drivers/media/platform/qcom/venus/vdec.c
+++ drivers/media/platform/qcom/venus/vdec.c
@@ -569,10 +569,7 @@ vdec_decoder_cmd(struct file *file, void *fh, struct v4l2_decoder_cmd *cmd)
 
 		fdata.buffer_type = HFI_BUFFER_INPUT;
 		fdata.flags |= HFI_BUFFERFLAG_EOS;
-		if (IS_V6(inst->core) && is_fw_rev_or_older(inst->core, 1, 0, 87))
-			fdata.device_addr = 0;
-		else
-			fdata.device_addr = 0xdeadb000;
+		fdata.device_addr = 0xdeadb000;
 
 		ret = hfi_session_process_buf(inst, &fdata);
 
