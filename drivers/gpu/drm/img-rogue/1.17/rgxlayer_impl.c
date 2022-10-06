ba6e4bf76822870ce82fb641ca0bf1ff437f810d
Łukasz Bartosik <ukaszb@google.com>
2022-10-06 05:10:39 +0000

    FIXUP: CHROMIUM: drm/img-rogue: Add 1.17 IMG PowerVR Rogue driver
    Create fixup.
    
    Signed-off-by: Łukasz Bartosik <ukaszb@google.com>
    Change-Id: I0af333fc5985734696edc1674bb30506ad6b003a

--- drivers/gpu/drm/img-rogue/1.17/rgxlayer_impl.c
+++ drivers/gpu/drm/img-rogue/1.17/rgxlayer_impl.c
@@ -51,7 +51,7 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "pmr.h"
 
 #if defined(PDUMP)
-#include <stdarg.h>
+#include <linux/stdarg.h>
 #endif
 
 void RGXMemCopy(const void *hPrivate,
