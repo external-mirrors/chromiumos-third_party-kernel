dc1525c3da13353a11e3cf12c2d97b693cff1802
Wen Gong <wgong@codeaurora.org>
2022-10-19 11:23:23 +0000

    ath10k: add atomic protection for device recovery
    Call trace/Kernel panic it fixes:- wmi_tx_beacons_iter – kernel panic
    
    Ath.git Upstream commit ID:- 5dadbe4e3718fb2214199b6dc7af1077fe14bf32
    
    Bug=b:241326632
    TEST Build successfully.
    
    Signed-off-by: Wen Gong <wgong@codeaurora.org>
    Change-Id: Icd86f47fec1929feddbc4f1ab37096bd37c4773d
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4023423
    Reviewed-by: Maharaja Kennadyrajan <mkenna@qualcomm.corp-partner.google.com>
    Commit-Queue: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    Tested-by: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    Auto-Submit: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    (cherry picked from commit 75c0c9cb2787f7833ba7ef0de6c4ade7b9de4458)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4066904
    Commit-Queue: Sucharita Viswanathan <sucharitav@google.com>
    Tested-by: Sucharita Viswanathan <sucharitav@google.com>

--- drivers/net/wireless/ath/ath10k/core.h
+++ drivers/net/wireless/ath/ath10k/core.h
@@ -1290,6 +1290,7 @@ int ath10k_core_start(struct ath10k *ar, enum ath10k_firmware_mode mode,
 		      const struct ath10k_fw_components *fw_components);
 int ath10k_wait_for_suspend(struct ath10k *ar, u32 suspend_opt);
 void ath10k_core_stop(struct ath10k *ar);
+void ath10k_core_start_recovery(struct ath10k *ar);
 int ath10k_core_register(struct ath10k *ar, u32 chip_id);
 void ath10k_core_unregister(struct ath10k *ar);
 int ath10k_core_fetch_board_file(struct ath10k *ar);
