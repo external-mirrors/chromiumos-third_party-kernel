ae33b105a931c077166723649d8b933440e49def
Ben Greear <greearb@candelatech.com>
2022-10-19 07:41:55 +0000

    ath10k: Don't iterate over not-sdata-in-driver interfaces.
    Call trace/Kernel panic it fixes:- wmi_tx_beacons_iter – kernel panic
    
    Ath.git Upstream commit ID:- ee06fcb98dcdc2713c83c29e9101b007a825cd7f
    
    Bug=b:241326632
    TEST=Build successfully.
    
    Signed-off-by: Ben Greear <greearb@candelatech.com>
    Change-Id: I963e58f5a7cad1d87721f28f23d9a77a2ab7c392
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4023422
    Auto-Submit: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    Reviewed-by: Maharaja Kennadyrajan <mkenna@qualcomm.corp-partner.google.com>
    Reviewed-by: Julan Hsu <julanhsu@google.com>
    Tested-by: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    Commit-Queue: RATHEESH SELVAKUMAR PAPPA <ratheeshs@google.com>
    (cherry picked from commit 34da58ffb8b7cfa82e07a7003a230d754b353f44)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4069380
    Tested-by: Sucharita Viswanathan <sucharitav@google.com>
    Commit-Queue: Sucharita Viswanathan <sucharitav@google.com>
    (cherry picked from commit cabb2b118cc03f96079fd26a66700df890ccaa34)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5127118
    Reviewed-by: Srinivasa Duvvuri <sduvvuri@google.com>

--- drivers/net/wireless/ath/ath10k/p2p.c
+++ drivers/net/wireless/ath/ath10k/p2p.c
@@ -150,7 +150,7 @@ void ath10k_p2p_noa_update_by_vdev_id(struct ath10k *ar, u32 vdev_id,
 	};
 
 	ieee80211_iterate_active_interfaces_atomic(ar->hw,
-						   IEEE80211_IFACE_ITER_NORMAL,
+						   ITER_NORMAL_FLAGS,
 						   ath10k_p2p_noa_update_vdev_iter,
 						   &arg);
 }
