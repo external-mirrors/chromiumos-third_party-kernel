46ebd389a4dcd0b1bdc7a45a932e319448a5c253
Brent Mendelsohn <mendiebm@gmail.com>
2022-10-24 18:42:27 +0100

    ASoC: amd: yc: Add Alienware m17 R5 AMD into DMI table
    [ Upstream commit d40b6529c6269cd5afddb1116a383cab9f126694 ]
    
    This model requires an additional detection quirk to enable the
    internal microphone - BIOS doesn't seem to support AcpDmicConnected
    (nothing in acpidump output).
    
    Link: https://bugzilla.kernel.org/show_bug.cgi?id=216590
    Signed-off-by: Brent Mendelsohn <mendiebm@gmail.com>
    Reviewed-by: Mario Limonciello <mario.limonciello@amd.com>
    Link: https://lore.kernel.org/r/20221024174227.4160-1-mendiebm@gmail.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    Stable-dep-of: b24e3590c94a ("ASoC: amd: yc: Fix non-functional mic on ASUS E1504FA")
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- sound/soc/amd/yc/acp6x-mach.c
+++ sound/soc/amd/yc/acp6x-mach.c
@@ -152,6 +152,13 @@ static const struct dmi_system_id yc_acp_quirk_table[] = {
 			DMI_MATCH(DMI_PRODUCT_NAME, "21D9"),
 		}
 	},
+	{
+		.driver_data = &acp6x_card,
+		.matches = {
+			DMI_MATCH(DMI_BOARD_VENDOR, "Alienware"),
+			DMI_MATCH(DMI_PRODUCT_NAME, "Alienware m17 R5 AMD"),
+		}
+	},
 	{}
 };
 
