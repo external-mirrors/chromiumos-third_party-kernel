8a7404795005a33f3f8fb0b6c2e8318e8f664057
Lukasz Majczak <lmajczak@google.com>
2022-10-25 12:11:35 +0000

    FIXUP: CHROMIUM: net: wwan: t7xx: Add NAPI support
    Change-Id: I90a84f1cf91f20b79e54ff5bb79989c9c9dd7559

--- drivers/net/wwan/t7xx/t7xx_netdev.c
+++ drivers/net/wwan/t7xx/t7xx_netdev.c
@@ -254,8 +254,8 @@ static void t7xx_init_netdev_napi(struct t7xx_ccmni_ctrl *ctlb)
 
 	for (i = 0; i < RXQ_NUM; i++) {
 		ctlb->napi[i] = &ctlb->hif_ctrl->rxq[i].napi;
-		netif_napi_add(&ctlb->dummy_dev, ctlb->napi[i],
-			       t7xx_dpmaif_napi_rx_poll);
+		netif_napi_add_weight(&ctlb->dummy_dev, ctlb->napi[i], t7xx_dpmaif_napi_rx_poll,
+				     NIC_NAPI_POLL_BUDGET);
 	}
 }
 
