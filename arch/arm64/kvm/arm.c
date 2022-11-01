63e801dee99bbe5fe02d33faea30970fadd0513e
Masami Hiramatsu <mhiramat@google.com>
2022-11-01 14:35:29 +0900

    CHROMIUM: KVM: arm64: Add support for the KVM uclamp service
    Implement the hypervisor side of the KVM uclamp interface.
    
    This service allows guest to set its uclamp value to the vCPU tasks
    in the host. The caller must specify the uclamp value in the range of
    0-1024, and uclamp.min must be smaller than uclamp.max.
    The uclamp value is agressively used if the host kernel runs with
    the EAS and schedutil. Thus this hypercall will be good for such
    case.
    
    BUG=b:230005239
    BUG=b:230582047
    UPSTREAM-TASK=b:256983979
    TEST=Installed and run autologin.py.
    
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Change-Id: If5a492cbde81511f3c98d8c49671e295402127fc
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3849762
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Joel Fernandes <joelaf@google.com>
    Commit-Queue: Joel Fernandes <joelaf@google.com>
    Kcr-patch: a7f58e7fd5cab24c5801dd0f8c8a9d52f1dae4e1519dc81f9dd409f4.patch

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -234,6 +234,7 @@ int kvm_vm_ioctl_check_extension(struct kvm *kvm, long ext)
 	case KVM_CAP_ARM_SYSTEM_SUSPEND:
 	case KVM_CAP_IRQFD_RESAMPLE:
 	case KVM_CAP_COUNTER_OFFSET:
+	case KVM_CAP_UCLAMP_SYNC:
 		r = 1;
 		break;
 	case KVM_CAP_SET_GUEST_DEBUG2:
