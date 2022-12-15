31528d5b1ba1b5fa84dcdf78aa80f086d433d9cf
David Stevens <stevensd@chromium.org>
2022-12-15 13:37:09 +0000

    BACKPORT: FROMLIST: dma-iommu: expose a few helper functions to module
    Expose a few helper functions from dma-iommu to the rest of the module.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: David Stevens <stevensd@chromium.org>
    Signed-off-by: Tomasz Nowicki <tnowicki@google.com>
    (am from https://lore.kernel.org/lkml/20210806103423.3341285-3-stevensd@google.com/)
    
    BUG=b:255912146
    TEST=boot brya-manatee with all devices behind virtio-iommu
    
    Change-Id: I5e6aab18004921f4e2dc4550b198efae3e0008ab

--- include/linux/dma-iommu.h
+++ include/linux/dma-iommu.h
@@ -49,7 +49,7 @@ dma_addr_t __iommu_dma_alloc_iova(struct iommu_domain *domain,
 				  size_t size, u64 dma_limit,
 				  struct device *dev);
 void __iommu_dma_free_iova(struct iommu_dma_cookie *cookie,
-		dma_addr_t iova, size_t size, struct page *freelist);
+		dma_addr_t iova, size_t size, struct iommu_iotlb_gather *gather);
 
 #else /* CONFIG_IOMMU_DMA */
 
