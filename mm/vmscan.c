92f37fbbaea311fe48f19fefc40aa86e5594f020
Koki Ryu <kokiryu@google.com>
2022-12-22 17:30:17 +0900

    CHROMIUM: MGLRU: sysfs notification using a kernfs_node
    CL:3936130 used sysfs_notify to notify the change of the memory
    generations to the user space. However, this change caused a lock
    dependency issue. The lock is taken in the retrieval of the
    corresponding kernfs_node, so we solve this problem by simply holding
    the node as a global variable.
    
    BUG=b:260967742
    TEST=Built.
      Run /google/data/ro/users/su/suleiman/tests/poll.c on /sys/kernel/mm/lru_gen/admin
      while generating memory pressure with /google/data/ro/users/su/suleiman/tests/memtoy,
      ensuring it returns a POLLPRI event.
      Also made sure kernel built with USE=lockdebug doesn't have warnings.
    Signed-off-by: Koki Ryu <kokiryu@google.com>
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Change-Id: I86aa889e360d219939c8e673067822cf340c0f01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4393685
    Commit-Queue: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Reviewed-by: Kameron Lutes <kalutes@chromium.org>
    Kcr-patch: 34194b7b90ddd08db74097ba7d7a0eeac1a9c9a2528f874d8e3f833f.patch

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -188,6 +188,8 @@ struct scan_control {
  */
 int vm_swappiness = 60;
 
+struct kernfs_node *lru_gen_admin_node;
+
 LIST_HEAD(shrinker_list);
 DECLARE_RWSEM(shrinker_rwsem);
 
@@ -4501,7 +4503,7 @@ static bool try_to_inc_max_seq(struct lruvec *lruvec, unsigned long max_seq,
 	struct lru_gen_folio *lrugen = &lruvec->lrugen;
 
 	VM_WARN_ON_ONCE(max_seq > READ_ONCE(lrugen->max_seq));
-
+        kernfs_notify(lru_gen_admin_node);
 	/* see the comment in iterate_mm_list() */
 	if (max_seq <= READ_ONCE(lruvec->mm_state.seq)) {
 		success = false;
@@ -6403,11 +6405,14 @@ void lru_gen_exit_memcg(struct mem_cgroup *memcg)
 
 static int __init init_lru_gen(void)
 {
+	struct kernfs_node *tmp;
 	BUILD_BUG_ON(MIN_NR_GENS + 1 >= MAX_NR_GENS);
 	BUILD_BUG_ON(BIT(LRU_GEN_WIDTH) <= MAX_NR_GENS);
 
 	if (sysfs_create_group(mm_kobj, &lru_gen_attr_group))
 		pr_err("lru_gen: failed to create sysfs group\n");
+	tmp = kernfs_find_and_get(mm_kobj->sd, "lru_gen");
+	lru_gen_admin_node = kernfs_find_and_get(tmp, "admin");
 
 	debugfs_create_file("lru_gen", 0644, NULL, NULL, &lru_gen_rw_fops);
 	debugfs_create_file("lru_gen_full", 0444, NULL, NULL, &lru_gen_ro_fops);
