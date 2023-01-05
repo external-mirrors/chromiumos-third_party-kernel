crrashh1542/android_kernel_microsoft_wsa

BACKPORT: FROMLIST: ASoC: hdmi-codec: Add event handler for hdmi TX
If the speaker and hdmi are connect to the same port of I2S,
when try to switch to speaker playback, we will find that hdmi
is always turned on automatically. The way of switching is
through SOC_DAPM_PIN_SWITCH, however, such events can not be
handled in hdmi-codec driver.

So add event handler for hdmi TX to solve the above issue.

Signed-off-by: Jiaxin Yu <jiaxin.yu@mediatek.com>
(am from https://patchwork.kernel.org/patch/13056551/)
(also found at https://lore.kernel.org/r/20221125094413.4940-2-jiaxin.yu@mediatek.com)
[downstream: This function interface is not necessary, the previous description is wrong, so make changes, it will be modified in V3 in upstream, so backport is currently used.]

BUG=b:242663561
TEST=boot to shell
UPSTREAM-TASK=b:263754578

Change-Id: I2b1d673e57448c956d3e378737b4ea69391f72b5
Signed-off-by: qinwentao <qinwentao@huaqin.corp-partner.google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4122064
Reviewed-by: Haikun Zhou <zhouhaikun5@huaqin.corp-partner.google.com>
Reviewed-by: Jiaxin Yu <jiaxin.yu@mediatek.corp-partner.google.com>
Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>
Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>

include/sound/hdmi-codec.h
@@ -82,6 +82,12 @@ struct hdmi_codec_ops {
 		       struct hdmi_codec_params *hparms);
 
 	/*
+	 * PCM trigger callback.
+	 * Optional
+	 */
+	int (*trigger)(struct device *dev, int cmd);
+
+	/*
 	 * Shuts down the audio stream.
 	 * Mandatory
 	 */

external-mirrors/chromiumos-third_party-kernel

(cherry picked from commit b1e11457204dbfac8d13d947723d1831f8d3b537)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4129284

include/sound/hdmi-codec.h
@@ -82,6 +82,12 @@ struct hdmi_codec_ops {
 		       struct hdmi_codec_params *hparms);
 
 	/*
+	 * PCM trigger callback.
+	 * Optional
+	 */
+	int (*trigger)(struct device *dev, int event);
+
+	/*
 	 * Shuts down the audio stream.
 	 * Mandatory
 	 */