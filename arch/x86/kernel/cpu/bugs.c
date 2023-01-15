cfc442d904ef60907a6a4bbded9451ce9f44c017
Aashish Sharma <shraash@google.com>
2023-01-15 13:10:59 +0530

    FIXUP: FROMLIST: sched: Add a coresched command line option
    0-day warning:
    
    >> arch/x86/kernel/cpu/bugs.c:51:20: warning: 'coresched_select' used
    but never defined
    
    Move function 'coresched_select' out of CONFIG_SYSFS conditional
    block.
    
    BUG=b:196543180,b:227610040
    TEST=build chromeos-kernelupstream-5.14-rc5 with debug and boot
         x86_64 without cross-HT MDS/L1TF issues
         (cat /sys/devices/system/cpu/vulnerabilities/mds and
         cat /sys/devices/system/cpu/vulnerabilities/l1tf)
    
    Change-Id: I24c961b9e093f22134f10d75ee7c717aa12d25af
    Signed-off-by: Aashish Sharma <shraash@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4166884
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Commit-Queue: Guenter Roeck <groeck@chromium.org>
    Tested-by: Guenter Roeck <groeck@chromium.org>

--- arch/x86/kernel/cpu/bugs.c
+++ arch/x86/kernel/cpu/bugs.c
@@ -2500,6 +2500,8 @@ ssize_t cpu_show_retbleed(struct device *dev, struct device_attribute *attr, cha
 	return cpu_show_common(dev, attr, buf, X86_BUG_RETBLEED);
 }
 
+#endif
+
 /*
  * When coresched=secure command line option is passed (default), disable core
  * scheduling if CPU does not have MDS/L1TF vulnerability.
@@ -2513,5 +2515,3 @@ static void __init coresched_select(void)
 		static_branch_disable(&sched_coresched_supported);
 #endif
 }
-
-#endif
