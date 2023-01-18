5cc3dc1e833882dad9315e8d0b99053dbdced686
Lee Jones <joneslee@google.com>
2023-01-18 15:49:24 +0000

    FROMGIT: ANDROID: usb: f_accessory: Check buffer size when initialised via composite
    When communicating with accessory devices via USBFS, the initialisation
    call-stack looks like:
    
      ConfigFS > Gadget ConfigFS > UDC > Gadget ConfigFS > Composite
    
    Eventually ending up in composite_dev_prepare() where memory for the
    data buffer is allocated and initialised.  The default size used for the
    allocation is USB_COMP_EP0_BUFSIZ (4k).  When handling bulk transfers,
    acc_ctrlrequest() needs to be able to handle buffers up to
    BULK_BUFFER_SIZE (16k).  Instead of adding new generic attributes to
    'struct usb_request' to track the size of the allocated buffer, we can
    simply split off the affected thread of execution to travel via a
    knowledgeable abstracted function acc_ctrlrequest_composite() where we
    can complete the necessary specific checks.
    
    Bug: 264029575
    Signed-off-by: Lee Jones <joneslee@google.com>
    Signed-off-by: Lee Jones <joneslee@google.com>
    (cherry picked from commit 9a8dcea6abcf74b5f463e0d2cb9ffc0d739a6a0d
     https://android.googlesource.com/kernel/common android13-5.15)
    
    BUG=b:271919375
    TEST=CQ
    
    Change-Id: I263b69453069541cf3a0c19a9e7d786f37a58a17
    Disallow-Recycled-Builds: test-failures
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4313625
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Zubin Mithra <zsm@chromium.org>
    
    [rebase66(tzungbi):
     Squashed:
       FIXUP: FROMGIT: ANDROID: usb: f_accessory: Check buffer size when initialised via composite
         (https://crrev.com/c/4805766)
    ]
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    
    Kcr-patch: 9aaeaa5d00f44418e0d474d9b6828f1d2bce891fb06837846774d656.patch
    Change-Id: I87a6c498f5ddb246f4c15dafdab06bf417a31a1c

--- drivers/usb/gadget/configfs.c
+++ drivers/usb/gadget/configfs.c
@@ -18,8 +18,8 @@
 #include <linux/usb/ch9.h>
 
 #ifdef CONFIG_USB_CONFIGFS_F_ACC
-extern int acc_ctrlrequest(struct usb_composite_dev *cdev,
-				const struct usb_ctrlrequest *ctrl);
+extern int acc_ctrlrequest_composite(struct usb_composite_dev *cdev,
+				     const struct usb_ctrlrequest *ctrl);
 void acc_disconnect(void);
 #endif
 static struct class *android_class;
@@ -1958,7 +1958,7 @@ static int android_setup(struct usb_gadget *gadget,
 
 #ifdef CONFIG_USB_CONFIGFS_F_ACC
 	if (value < 0)
-		value = acc_ctrlrequest(cdev, c);
+		value = acc_ctrlrequest_composite(cdev, c);
 #endif
 
 	if (value < 0)
