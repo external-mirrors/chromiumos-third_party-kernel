ac59940cde1d22fbcd6e6dd78cbec10862c210af
Sergey Senozhatsky <senozhatsky@chromium.org>
2023-01-25 18:55:03 +0900

    CHROMIUM: vcamtest: split add_buffer test
    Split it into two functions. One function has only one operation
    which adds the buffers. The other function has compound operations:
    first operation adds buffer, second operation uses buffer and third
    operation removes buffer.
    
    TEST=vcamtest
    BUG=none
    
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Change-Id: I396dd95f18ea78854a1ef2647802c22c7b95d1a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4190435
    Reviewed-by: Hidenori Kobayashi <hidenorik@chromium.org>
    Commit-Queue: Tomasz Figa <tfiga@chromium.org>

--- tools/kcam/vcam-test/main.c
+++ tools/kcam/vcam-test/main.c
@@ -1812,7 +1812,7 @@ out:
 	return ret;
 }
 
-static int test_add_buffer(struct libkc *cam)
+static int test_compound_buffer_operations(struct libkc *cam)
 {
 	struct libkc_operation *lco = NULL;
 	struct libkc_dmabuf *buf = NULL;
@@ -1823,7 +1823,7 @@ static int test_add_buffer(struct libkc *cam)
 	char *read_buffer;
 	int ret;
 
-	pr_info("Test ADD buffers\n");
+	pr_info("Test compound buffer operations\n");
 
 	entity = libkc_entity_lookup_by_name(cam, VCAM_DMA_IMPORT_ENTITY_NAME);
 	if (!entity) {
@@ -1839,7 +1839,170 @@ static int test_add_buffer(struct libkc *cam)
 		goto out;
 	}
 
-	lco = libkc_operation_get(2);
+	lco = libkc_operation_get(3);
+	if (!lco) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	read_buffer = calloc(1, READ_BUFFER_SIZE);
+	if (!read_buffer) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	/* Import (ADD) buffer */
+	op = libkc_operation_at(lco, 0);
+	if (!op) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	op->operation_type		= CAM_OPERATION_TYPE_ADD;
+	op->operation_add.id		= 1;
+	op->operation_add.fence_out	= 0;
+	op->operation_add.flags		= 0;
+	op->operation_add.delay_ns	= 0;
+	op->operation_add.entity	= entity->id;
+	op->operation_add.mode		= CAM_DEPENDENCY_WEAK_ORDER;
+
+	rw_list = libkc_rw_list_get(1);
+	if (!rw_list) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	op->operation_add.rd_wr_list	= (uint64_t)rw_list;
+	rw = libkc_rw_instruction_at(rw_list, 0);
+	if (!rw) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	rw->type	= CAM_DMABUF_INSTRUCTION;
+	rw->db.op	= CAM_DMABUF_OP_ADD;
+	rw->db.dma_fd	= buf->fd;
+	rw->db.buf_id	= 1;
+
+	/* Use imported buffer */
+	op = libkc_operation_at(lco, 1);
+	if (!op) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	op->operation_type		= CAM_OPERATION_TYPE_ADD;
+	op->operation_add.id		= 2;
+	op->operation_add.fence_out	= 0;
+	op->operation_add.flags		= 0;
+	op->operation_add.delay_ns	= 0;
+	op->operation_add.entity	= entity->id;
+	op->operation_add.mode		= CAM_DEPENDENCY_STRICT_ORDER;
+	op->operation_add.deps[0].type	= CAM_DEPENDENCY_OP;
+	op->operation_add.deps[0].id	= 1;
+
+	rw_list = libkc_rw_list_get(1);
+	if (!rw_list) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	op->operation_add.rd_wr_list	= (uint64_t)rw_list;
+	rw = libkc_rw_instruction_at(rw_list, 0);
+	if (!rw) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	rw->type	= CAM_READ_INSTRUCTION;
+	rw->rd.reg	= 42;
+	rw->rd.size	= READ_BUFFER_SIZE;
+	rw->rd.dbuf	= 1;
+	rw->rd.ptr	= (uint64_t)read_buffer;
+
+	/* Release (REMOVE) buffer */
+	op = libkc_operation_at(lco, 2);
+	if (!op) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	op->operation_type		= CAM_OPERATION_TYPE_ADD;
+	op->operation_add.id		= 3;
+	op->operation_add.fence_out	= 0;
+	op->operation_add.flags		= 0;
+	op->operation_add.delay_ns	= 0;
+	op->operation_add.entity	= entity->id;
+	op->operation_add.mode		= CAM_DEPENDENCY_STRICT_ORDER;
+	op->operation_add.deps[0].type	= CAM_DEPENDENCY_OP;
+	op->operation_add.deps[0].id	= 2;
+
+	rw_list = libkc_rw_list_get(1);
+	if (!rw_list) {
+		ret = -ENOMEM;
+		goto out;
+	}
+
+	op->operation_add.rd_wr_list	= (uint64_t)rw_list;
+	rw = libkc_rw_instruction_at(rw_list, 0);
+	if (!rw) {
+		ret = -EINVAL;
+		goto out;
+	}
+
+	rw->type	= CAM_DMABUF_INSTRUCTION;
+	rw->db.op	= CAM_DMABUF_OP_REMOVE;
+	rw->db.dma_fd	= CAM_RW_INSTRUCTION_NO_BUFFER;
+	rw->db.buf_id	= 1;
+
+	ret = libkc_operation_ioctl(cam, lco);
+	if (ret)
+		goto out;
+
+	ret = read_operations_completion_events(cam, 3);
+	if (ret != 3) {
+		pr_err("Invalid number of completions %d\n", ret);
+		ret = -EINVAL;
+		goto out;
+	} else {
+		ret = 0;
+	}
+
+out:
+	libkc_dmabuf_put(buf);
+	libkc_operation_put(lco);
+	free(read_buffer);
+	return ret;
+}
+
+static int test_add_buffer(struct libkc *cam)
+{
+	struct libkc_operation *lco = NULL;
+	struct libkc_dmabuf *buf = NULL;
+	struct cam_rw_instruction *rw;
+	struct libkc_rw_list *rw_list;
+	struct obj_entity *entity;
+	struct cam_operation *op;
+	char *read_buffer;
+	int ret;
+
+	pr_info("Test ADD buffer\n");
+
+	entity = libkc_entity_lookup_by_name(cam, VCAM_DMA_IMPORT_ENTITY_NAME);
+	if (!entity) {
+		pr_err("Unable to lookup `%s` entity\n",
+		       VCAM_DMA_IMPORT_ENTITY_NAME);
+		return -EINVAL;
+	}
+
+	buf = libkc_dmabuf_get(cam, 4);
+	if (!buf) {
+		pr_err("Failed to create buffer\n");
+		ret = -EINVAL;
+		goto out;
+	}
+
+	lco = libkc_operation_get(1);
 	if (!lco) {
 		ret = -EINVAL;
 		goto out;
@@ -1883,49 +2046,14 @@ static int test_add_buffer(struct libkc *cam)
 	rw->db.dma_fd	= buf->fd;
 	rw->db.buf_id	= 1;
 
-	op = libkc_operation_at(lco, 1);
-	if (!op) {
-		ret = -EINVAL;
-		goto out;
-	}
-
-	op->operation_type		= CAM_OPERATION_TYPE_ADD;
-	op->operation_add.id		= 2;
-	op->operation_add.fence_out	= 0;
-	op->operation_add.flags		= 0;
-	op->operation_add.delay_ns	= 0;
-	op->operation_add.entity	= entity->id;
-	op->operation_add.mode		= CAM_DEPENDENCY_STRICT_ORDER;
-	op->operation_add.deps[0].type	= CAM_DEPENDENCY_OP;
-	op->operation_add.deps[0].id	= 1;
-
-	rw_list = libkc_rw_list_get(1);
-	if (!rw_list) {
-		ret = -ENOMEM;
-		goto out;
-	}
-
-	op->operation_add.rd_wr_list	= (uint64_t)rw_list;
-	rw = libkc_rw_instruction_at(rw_list, 0);
-	if (!rw) {
-		ret = -EINVAL;
-		goto out;
-	}
-
-	rw->type	= CAM_READ_INSTRUCTION;
-	rw->rd.reg	= 42;
-	rw->rd.size	= READ_BUFFER_SIZE;
-	rw->rd.dbuf	= 1;
-	rw->rd.ptr	= (uint64_t)read_buffer;
-
 	ret = libkc_operation_ioctl(cam, lco);
 	if (ret)
 		goto out;
 
 	MAY_EXIT_AT();
 
-	ret = read_operations_completion_events(cam, 2);
-	if (ret != 2) {
+	ret = read_operations_completion_events(cam, 1);
+	if (ret != 1) {
 		pr_err("Invalid number of completions %d\n", ret);
 		ret = -EINVAL;
 		goto out;
@@ -2188,6 +2316,12 @@ static void *thread_fn(void *arg)
 		goto out;
 	}
 
+	ret = test_compound_buffer_operations(cam);
+	if (ret) {
+		pr_err("FATAL: failure test_compound_buffer_operations()\n");
+		goto out;
+	}
+
 	ret = test_add_buffer(cam);
 	if (ret) {
 		pr_err("FATAL: failure test_add_buffer()\n");
@@ -2321,6 +2455,12 @@ int main(int argc, char *argv[])
 	ret = test_export_import_operations(cam);
 	if (ret) {
 		pr_err("FATAL: failure test_export_import_operations()\n");
+		return ret;
+	}
+
+	ret = test_compound_buffer_operations(cam);
+	if (ret) {
+		pr_err("FATAL: failure test_compound_buffer_operations()\n");
 		return ret;
 	}
 
