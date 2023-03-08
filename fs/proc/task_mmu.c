70c822336e783d12445c6d38b576ad4b93fefd7f
Łukasz Bartosik <ukaszb@google.com>
2023-03-08 22:56:58 +0000

    FIXUP: CHROMIUM: mm: per-process reclaim
    Create fixup.
    
    Signed-off-by: Łukasz Bartosik <ukaszb@google.com>
    Change-Id: Idb2e002376e5856fef0de31182a04541bb00df04
    Kcr-patch: fixups/CHROMIUM: mm: per-process reclaim.patch

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1826,7 +1826,7 @@ static int deactivate_pte_range(pmd_t *pmd, unsigned long addr,
 		}
 
 		pmdp_test_and_clear_young(vma, addr, pmd);
-		deactivate_page(page);
+		folio_deactivate(page_folio(page));
 huge_unlock:
 		spin_unlock(ptl);
 		return 0;
@@ -1876,7 +1876,7 @@ regular_page:
 			continue;
 
 		ptep_test_and_clear_young(vma, addr, pte);
-		deactivate_page(page);
+		folio_deactivate(page_folio(page));
 	}
 	pte_unmap_unlock(orig_pte, ptl);
 	cond_resched();
