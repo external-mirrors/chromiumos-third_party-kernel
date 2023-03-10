489835e34d3090f8aec2afc86ac7d8021ca4cbbe
Dmytro Maluka <dmaluka@google.com>
2023-03-10 09:28:33 +0000

    Revert "CHROMIUM: pkvm: x86: Add shadow IOMMU page tables pool"
    This reverts commit bcce4bcdb8153db73686b2e78c4228e96beaad9c.
    
    Reason: reverting the older version of the legacy IOMMU patchset
    in order to cleanly apply the latest patches from Intel github
    https://github.com/intel-staging/pKVM-IA which include the newer
    version of the legacy IOMMU patchset, to make the gerrit pkvm-ia-5.19
    branch aligned with Intel github main branch.
    
    BUG=b:255568785
    TEST=Compile and boot
    
    Change-Id: Ied494ce4da47174e69086d820cfe53e377c13ac0
    Signed-off-by: Dmytro Maluka <dmaluka@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4329376
    Reviewed-by: Tomasz Nowicki <tnowicki@google.com>

--- arch/x86/kvm/vmx/pkvm/hyp/ept.h
+++ arch/x86/kvm/vmx/pkvm/hyp/ept.h
@@ -5,7 +5,6 @@
 #ifndef __PKVM_EPT_H
 #define __PKVM_EPT_H
 
-#include <gfp.h>
 #include "pkvm_hyp.h"
 
 #define HOST_EPT_DEF_MEM_PROT   (VMX_EPT_RWX_MASK |				\
@@ -50,7 +49,6 @@ void pkvm_shadow_clear_suppress_ve(struct kvm_vcpu *vcpu, unsigned long gfn);
 int pkvm_pgstate_pgt_init(struct pkvm_shadow_vm *vm);
 void pkvm_pgstate_pgt_deinit(struct pkvm_shadow_vm *vm);
 bool is_pgt_ops_ept(struct pkvm_pgtable *pgt);
-void *ept_zalloc_page(struct pkvm_pool *pool);
 
 static inline bool is_valid_eptp(u64 eptp)
 {
