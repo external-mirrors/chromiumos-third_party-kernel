89e86def188304938beeb2ee4e6cf475fd2683c0
Suleiman Souhlal <suleiman@google.com>
2023-03-13 11:42:00 +0900

    CHROMIUM: MGLRU: Fix crash when writing to sysfs MGLRU lru_gen/admin file.
    The task's reclaim_state needs to be set.
    
    Fixes: 0a33ee84ae07 ("CHROMIUM: Reland of add implementation of MGLRU in sysfs")
    
    BUG=b:272641902
    TEST=Built.
      "tast run dut ui.DesksCUJ" doesn't crash anymore (on other kernel versions)
    
    Signed-off-by: Suleiman Souhlal <suleiman@google.com>
    Change-Id: Iad8887294bad3b4b5393eb1cbc04d69346e8c35b
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4393683
    Reviewed-by: Kameron Lutes <kalutes@chromium.org>
    Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>
    Auto-Submit: Suleiman Souhlal <suleiman@chromium.org>
    Tested-by: Suleiman Souhlal <suleiman@chromium.org>
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -5981,6 +5981,8 @@ static ssize_t store_lru_gen_admin(struct kobject *kobj, struct kobj_attribute *
 	next = buf;
 	next[len] = '\0';
 
+	set_task_reclaim_state(current, &sc.reclaim_state);
+
 	while ((cur = strsep(&next, ",;\n"))) {
 		int n;
 		int end;
@@ -6007,6 +6009,7 @@ static ssize_t store_lru_gen_admin(struct kobject *kobj, struct kobj_attribute *
 			break;
 	}
 
+	set_task_reclaim_state(current, NULL);
 	kvfree(buf);
 
 	return err ? : len;
