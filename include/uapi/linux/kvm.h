6eae60bbc10962fc8b756def3c89503c58cce108
David Dai <davidai@google.com>
2023-05-18 16:02:15 +0900

    BACKPORT: FROMLIST: kvm: arm64: Add support for get_freqtbl service
    This service allows guests to query the host for the frequency table
    of the CPU that the vCPU is currently running on.
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>
    (am from https://patchwork.kernel.org/patch/13194985/)
    (also found at https://lore.kernel.org/r/20230330224348.1006691-5-davidai@google.com)
    
    Conflicts:
       include/linux/arm-smccc.h
    Changes:
       Change the hypercall ID so that it doesn't share the possible new hypercall
       IDs.
       Remove KVM_REG_ARM_VENDOR_HYP_BIT_CPUFREQ_TBL because it was assigned to
       wrong number and the correct number is out of 64bit.
       GET_CPUFREQ_TBL hypercall is hidden from UAPI and always available.
    
    BUG=b:249685960
    TEST=Build, deploy on krane, and run
         android-sh -c 'ls /sys/devices/system/cpu/cpufreq/policy0'
    UPSTREAM-TASK=b:256983979
    
    Change-Id: Ic537a3deda1d87048efe7f8d44e2ffcb6addb453
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4546354
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1192,6 +1192,7 @@ struct kvm_ppc_resize_hpt {
 #define KVM_CAP_COUNTER_OFFSET 227
 #define KVM_CAP_GET_CUR_CPUFREQ 512
 #define KVM_CAP_UTIL_HINT 513
+#define KVM_CAP_GET_CPUFREQ_TBL 514
 
 #ifdef KVM_CAP_IRQ_ROUTING
 
