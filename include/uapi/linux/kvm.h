3fcf3f46dd3ce34bd13a94ea1b6e584d4b1aee12
David Dai <davidai@google.com>
2023-05-18 16:02:14 +0900

    BACKPORT: FROMLIST: kvm: arm64: Add support for util_hint service
    This service allows guests to send the utilization of workoads on its vCPUs
    to the host. Utilization is represented as an arbitrary value of 0-1024
    where 1024 represents the highest performance point normalized for
    frequency and architecture across all CPUs. This hint is used by
    the host for scheduling vCPU threads and deciding CPU frequency.
    
    Co-developed-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: Saravana Kannan <saravanak@google.com>
    Signed-off-by: David Dai <davidai@google.com>
    (am from https://patchwork.kernel.org/patch/13194984/)
    (also found at https://lore.kernel.org/r/20230330224348.1006691-4-davidai@google.com)
    
    Conflicts:
       include/linux/arm-smccc.h
    
    Changes:
        Change the hypercall ID so that it doesn't share the possible new hypercall
        IDs.
        Remove KVM_REG_ARM_VENDOR_HYP_BIT_UTIL_HINT because it was assigned to
        wrong number and the correct number is out of 64bit.
        UTIL_HINT hypercall is hidden from UAPI and always available.
    
    BUG=b:249685960
    TEST=Build
    UPSTREAM-TASK=b:256983979
    
    Change-Id: I2072bf7b9b4053b469b0a1f3e71def4d70f30fc6
    Signed-off-by: Masami Hiramatsu <mhiramat@google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4546353
    Reviewed-by: Suleiman Souhlal <suleiman@chromium.org>
    Kcr-patch: 90c6e30bd939593dcaeac999b2bcbcc6c05188e509324e956efc9589.patch
    Kcr-patch: 18edd2f7325f3bb8597a505c10d0a11d61ae6188be13730cc6452104.patch

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -918,6 +918,7 @@ struct kvm_enable_cap {
 #define KVM_CAP_GUEST_MEMFD 234
 #define KVM_CAP_VM_TYPES 235
 #define KVM_CAP_GET_CUR_CPUFREQ 512
+#define KVM_CAP_UTIL_HINT 513
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
