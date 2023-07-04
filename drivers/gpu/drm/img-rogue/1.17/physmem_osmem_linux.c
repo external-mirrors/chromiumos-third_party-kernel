37c0a245492aaa5d459e3fd58bee17dd18ff4b35
Luigi Santivetti <luigi.santivetti@imagination.corp-partner.google.com>
2023-07-04 15:25:06 +0100

    CHROMIUM: img-rogue/1.17: security fixes in PMR related code
    Address PVRSRVBridgeChangeSparseMem vulnerabilities Correct
    bounds checking performed when accessing page array in
    PMRChangeSparseMemOSMem() and PMRChangeSparseMemLocalMem().
    Prevent PMR_ChangeSparseMem() from changing the layout of a
    sparse allocation while it is mapped.
    
    CVE-2023-21216
    CVE-2023-21218
    CVE-2023-21228
    
    BUG=b:290608737
    TEST=none
    
    Change-Id: Id232dc0fccb041f771d0af55e7a37529cb1571c7
    Signed-off-by: Luigi Santivetti <luigi.santivetti@imagination.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4694964
    Reviewed-by: Eizan Miyamoto <eizan@chromium.org>
    Tested-by: Eizan Miyamoto <eizan@chromium.org>
    Commit-Queue: Eizan Miyamoto <eizan@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5198638
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Tested-by: Pin-yen Lin <treapking@chromium.org>
    Commit-Queue: Pin-yen Lin <treapking@chromium.org>

--- drivers/gpu/drm/img-rogue/1.17/physmem_osmem_linux.c
+++ drivers/gpu/drm/img-rogue/1.17/physmem_osmem_linux.c
@@ -3456,7 +3456,7 @@ PMRChangeSparseMemOSMem(PMR_IMPL_PRIVDATA pPriv,
 			{
 				uiFreepgidx = pai32FreeIndices[ui32Loop];
 
-				if (uiFreepgidx > (psPMRPageArrayData->uiTotalNumOSPages >> uiOrder))
+				if (uiFreepgidx >= (psPMRPageArrayData->uiTotalNumOSPages >> uiOrder))
 				{
 					eError = PVRSRV_ERROR_DEVICEMEM_OUT_OF_RANGE;
 					goto e0;
@@ -3487,7 +3487,7 @@ PMRChangeSparseMemOSMem(PMR_IMPL_PRIVDATA pPriv,
 	{
 		uiAllocpgidx = pai32AllocIndices[ui32Loop];
 
-		if (uiAllocpgidx > (psPMRPageArrayData->uiTotalNumOSPages >> uiOrder))
+		if (uiAllocpgidx >= (psPMRPageArrayData->uiTotalNumOSPages >> uiOrder))
 		{
 			eError = PVRSRV_ERROR_DEVICEMEM_OUT_OF_RANGE;
 			goto e0;
