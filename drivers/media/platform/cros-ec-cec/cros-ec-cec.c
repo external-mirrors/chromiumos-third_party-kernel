26df37b1fdc94fb816a0c73041ef6b4d50f4b000
Reka Norman <rekanorman@chromium.org>
2023-08-25 12:44:02 +1000

    BACKPORT: FROMGIT: media: cros-ec-cec: Add Dibbi to the match table
    Dibbi has two HDMI ports which support CEC:
    Port D is EC port 0
    Port B is EC port 1
    
    Signed-off-by: Reka Norman <rekanorman@chromium.org>
    Signed-off-by: Hans Verkuil <hverkuil-cisco@xs4all.nl>
    (cherry picked from commit feaf39f11e136f51c60ddf22d3a09d4031cbb5df
     git://linuxtv.org/media_stage.git master)
    
    Conflicts:
       drivers/media/platform/cros-ec-cec/cros-ec-cec.c
    
    BUG=b:270507438
    TEST=CEC works on dibbi
    
    Change-Id: I2f28877581b914072e42413298b17ad641b0c0b3
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4857669
    Tested-by: Reka Norman <rekanorman@chromium.org>
    Reviewed-by: Sam McNally <sammc@chromium.org>
    Commit-Queue: Reka Norman <rekanorman@chromium.org>
    (cherry picked from commit 6201e4b0a176d5af5dd803297aadcfbad3759417)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4862323
    Reviewed-by: Ricky Chang <rickytlchang@chromium.org>
    Reviewed-by: Reka Norman <rekanorman@chromium.org>
    Commit-Queue: Ricky Chang <rickytlchang@chromium.org>
    Tested-by: ChiJen Chen <chi-jen.chen@quanta.corp-partner.google.com>

--- drivers/media/platform/cros-ec-cec/cros-ec-cec.c
+++ drivers/media/platform/cros-ec-cec/cros-ec-cec.c
@@ -295,11 +295,14 @@ struct cec_dmi_match {
 };
 
 static const char *const fizz_conns[] = { "Port B", NULL };
+static const char *const dibbi_conns[] = { "Port D", "Port B", NULL };
 static const char *const boxy_conns[] = { "Port D", NULL };
 
 static const struct cec_dmi_match cec_dmi_match_table[] = {
 	/* Google Fizz */
 	{ "Google", "Fizz", "0000:00:02.0", fizz_conns },
+	/* Google Dibbi */
+	{ "Google", "Dibbi", "0000:00:02.0", dibbi_conns },
 	/* Google Boxy */
 	{ "Google", "Boxy", "0000:00:02.0", boxy_conns },
 };
