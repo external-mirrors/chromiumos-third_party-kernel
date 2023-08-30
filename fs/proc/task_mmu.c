018024272b19e299681868c83b08cf7ca30627d3
Bartlomiej Bujak <sn-support@semihalf.com>
2023-08-30 18:05:26 +0200

    FIXUP: CHROMIUM: mm: per-process reclaim
    Create fixup.
    
    Change-Id: I886f30de8d24da45aee801e727f44bcc8420617e
    Signed-off-by: Bartlomiej Bujak <sn-support@semihalf.com>
    Kcr-depends-on-sha: ef3ae226162687a23569f414198c414e02df2a36: feda5c393a6c843c7bf1fc49e1381e2d3822b564
    Kcr-patch: fixups/FIXUP: CHROMIUM: mm: per-process reclaim for mm topic.patch

--- fs/proc/task_mmu.c
+++ fs/proc/task_mmu.c
@@ -1833,8 +1833,6 @@ huge_unlock:
 	}
 
 regular_page:
-	if (pmd_trans_unstable(pmd))
-		return 0;
 
 	orig_pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	for (pte = orig_pte; addr < end; pte++, addr += PAGE_SIZE) {
@@ -1953,8 +1951,6 @@ huge_unlock:
 	}
 
 regular_page:
-	if (pmd_trans_unstable(pmd))
-		return 0;
 
 	orig_pte = pte_offset_map_lock(vma->vm_mm, pmd, addr, &ptl);
 	for (pte = orig_pte; addr < end; pte++, addr += PAGE_SIZE) {
