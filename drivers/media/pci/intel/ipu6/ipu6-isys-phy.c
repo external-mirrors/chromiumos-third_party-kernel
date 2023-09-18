8d12640e70708d8faa2cf3723534c1fd4124c221
Bartlomiej Bujak <sn-support@semihalf.com>
2023-09-18 15:11:48 +0200

    FIXUP: CHROMIUM: media: intel-ipu6: copy IPU6 driver from v5.4 branch
    Change-Id: I1d9dbaf8f4d911cd27455390522be6da9369febf
    Signed-off-by: Bartlomiej Bujak <sn-support@semihalf.com>
    Kcr-depends-on-sha: adb2dcd5f2d49d3ba3171160fabd4be0d4b2a86c
    Kcr-depends-on-sha: bda8953e8c3e7ecbbf6cb1be11790496300e3961
    Kcr-patch: fixups/FIXUP: CHROMIUM: media: intel-ipu6: copy IPU6 driver from v5.4 branch.patch

--- drivers/media/pci/intel/ipu6/ipu6-isys-phy.c
+++ drivers/media/pci/intel/ipu6/ipu6-isys-phy.c
@@ -504,11 +504,11 @@ int ipu6_isys_phy_common_init(struct ipu_isys *isys)
 	struct ipu_bus_device *adev = to_ipu_bus_device(&isys->adev->dev);
 	struct ipu_device *isp = adev->isp;
 	void __iomem *isp_base = isp->base;
-	struct v4l2_async_subdev *asd;
+	struct v4l2_async_connection *asd;
 	struct sensor_async_subdev *s_asd;
 	unsigned int i;
 
-	list_for_each_entry(asd, &isys->notifier.asd_list, asd_list) {
+	list_for_each_entry(asd, &isys->notifier.done_list, asc_entry) {
 		s_asd = container_of(asd, struct sensor_async_subdev, asd);
 		phy_id = s_asd->csi2.port / 4;
 		phy_base = isp_base + IPU6_ISYS_PHY_BASE(phy_id);
@@ -562,12 +562,12 @@ int ipu6_isys_phy_config(struct ipu_isys *isys)
 	struct ipu_device *isp = adev->isp;
 	void __iomem *isp_base = isp->base;
 	const struct phy_reg **phy_config_regs;
-	struct v4l2_async_subdev *asd;
+	struct v4l2_async_connection *asd;
 	struct sensor_async_subdev *s_asd;
 	struct ipu_isys_csi2_config cfg;
 	int i;
 
-	list_for_each_entry(asd, &isys->notifier.asd_list, asd_list) {
+	list_for_each_entry(asd, &isys->notifier.done_list, asc_entry) {
 		s_asd = container_of(asd, struct sensor_async_subdev, asd);
 		cfg.port = s_asd->csi2.port;
 		cfg.nlanes = s_asd->csi2.nlanes;
