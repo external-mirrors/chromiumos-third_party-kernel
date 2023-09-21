crrashh1542/android_kernel_microsoft_wsa

CHROMIUM: msm: camera: isp: Copy packet and cmd buffer header in kernel
After getting the mapped buffers through cam_mem_get_cpu_buf()
in kernel, userspace is still able to access those buffers.
So, the validation of offsets and sizes does not have any
advantage if userspace can alter those parameters after we
have validated them. This change copies the information in kernel
before we validate that.

BUG=b:264148881
UPSTREAM-TASK=b:274966069
TEST=cros_camera_connector_test
--gtest_filter=ConnectorTest/CaptureTest.OneFrame/NV12_640x480_30fps
TEST=Run the poc like below, kill the chrome process and quickly run
the poc:
- localhost /usr/local # chmod +x cros_cam_jpeg_race
- localhost /usr/local # su chronos
- chronos@localhost /usr/local $ kill -9 `ps -ef |
grep "/opt/google/chrome/chrome --use-gl=egl" | grep -v grep |
awk '{print $2}'`
- chronos@localhost /usr/local $ ./cros_cam_jpeg_race

Change-Id: Ic9822b25e27a07eb196a313b83fd72f2df501b68
Signed-off-by: Stuti Saxena <stutsaxe@qualcomm.corp-partner.google.com>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4290543
Commit-Queue: Sergey Senozhatsky <senozhatsky@chromium.org>
Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
Tested-by: Sergey Senozhatsky <senozhatsky@chromium.org>

drivers/media/platform/camx/cam_utils/cam_common_util.c
@@ -14,9 +14,14 @@
 #include <linux/string.h>
 #include <linux/types.h>
 #include <linux/slab.h>
-
+#include <linux/timer.h>
+#include <linux/completion.h>
+#include <linux/module.h>
+#include <linux/iopoll.h>
+#include <linux/moduleparam.h>
 #include "cam_common_util.h"
 #include "cam_debug_util.h"
+#include "cam_hw.h"
 
 int cam_common_util_get_string_index(const char **strings,
 	uint32_t num_strings, char *matching_string, uint32_t *index)
@@ -62,3 +67,24 @@ ktime_t cam_common_util_get_curr_timestamp(void)
 	return ktime_get_boottime();
 }
 
+void *cam_common_mem_kdup(void *from, size_t len)
+{
+	void *to = kvzalloc(len, GFP_KERNEL);
+
+	if (!to) {
+		CAM_ERR(CAM_UTIL, "Failed to allocate header memory");
+		return to;
+	}
+
+	CAM_DBG(CAM_UTIL, "Allocate and copy header with size: %d", len);
+	memcpy(to, from, len);
+
+	return to;
+}
+EXPORT_SYMBOL(cam_common_mem_kdup);
+
+void cam_common_mem_free(void *memory)
+{
+	kvfree(memory);
+}
+EXPORT_SYMBOL(cam_common_mem_free);

external-mirrors/chromiumos-third_party-kernel

[senozhatsky: fixed Wformat build errors]
(cherry picked from commit f01e359d441be6d9613f4a9dc280f972e7a7d8de)
FIX4
Change-Id: I3c1389f804d4ba8d9bfb980d59aba7e48dd1d616
Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4495147
Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>

drivers/media/platform/camx/cam_utils/cam_common_util.c
@@ -14,9 +14,14 @@
 #include <linux/string.h>
 #include <linux/types.h>
 #include <linux/slab.h>
-
+#include <linux/timer.h>
+#include <linux/completion.h>
+#include <linux/module.h>
+#include <linux/iopoll.h>
+#include <linux/moduleparam.h>
 #include "cam_common_util.h"
 #include "cam_debug_util.h"
+#include "cam_hw.h"
 
 int cam_common_util_get_string_index(const char **strings,
 	uint32_t num_strings, char *matching_string, uint32_t *index)
@@ -62,3 +67,24 @@ ktime_t cam_common_util_get_curr_timestamp(void)
 	return ktime_get_boottime();
 }
 
+void *cam_common_mem_kdup(void *from, size_t len)
+{
+	void *to = kvzalloc(len, GFP_KERNEL);
+
+	if (!to) {
+		CAM_ERR(CAM_UTIL, "Failed to allocate header memory");
+		return to;
+	}
+
+	CAM_DBG(CAM_UTIL, "Allocate and copy header with size: %zu", len);
+	memcpy(to, from, len);
+
+	return to;
+}
+EXPORT_SYMBOL(cam_common_mem_kdup);
+
+void cam_common_mem_free(void *memory)
+{
+	kvfree(memory);
+}
+EXPORT_SYMBOL(cam_common_mem_free);