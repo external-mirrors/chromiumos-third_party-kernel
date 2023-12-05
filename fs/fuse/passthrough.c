64ce6affe30b88f0c28c4791d8d38251d7921a0d
Bartłomiej Bujak <bbujak@google.com>
2023-12-05 14:40:25 +0000

    FIXUP: FROMLIST: fuse: Introduce passthrough for mmap
    Change-Id: Ib0dc250778afe5c9d72297fba9318139dbdba34c
    Signed-off-by: Bartłomiej Bujak <bbujak@google.com>
    Kcr-depends-on-sha: 8367c65d1e63b05c7b939696fae640a4551f2229: 12cd44023651666bd44baa36a5c999698890debb
    Kcr-patch: fixups/FIXUP: FROMLIST: fuse: Introduce passthrough for mmap - i_mtime.patch

--- fs/fuse/passthrough.c
+++ fs/fuse/passthrough.c
@@ -168,11 +168,11 @@ ssize_t fuse_passthrough_mmap(struct file *file, struct vm_area_struct *vma)
 	if (file->f_flags & O_NOATIME)
 		return ret;
 
-	if ((!timespec64_equal(&fuse_inode->i_mtime,
-			       &passthrough_inode->i_mtime) ||
+	if ((!timespec64_equal(&fuse_inode->__i_mtime,
+			       &passthrough_inode->__i_mtime) ||
 	     !timespec64_equal(&fuse_inode->__i_ctime,
 			       &passthrough_inode->__i_ctime))) {
-		fuse_inode->i_mtime = passthrough_inode->i_mtime;
+		fuse_inode->__i_mtime = passthrough_inode->__i_mtime;
 		fuse_inode->__i_ctime = passthrough_inode->__i_ctime;
 	}
 	touch_atime(&file->f_path);
