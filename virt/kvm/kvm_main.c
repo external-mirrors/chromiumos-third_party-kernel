80ab240ad89db9f697519102d57611ba4374d27d
Yan Zhao <yan.y.zhao@intel.com>
2024-01-05 17:13:46 +0800

    BACKPORT: FROMLIST: KVM: Introduce a new memslot flag KVM_MEM_NON_COHERENT_DMA
    Introduce a new flag KVM_MEM_NON_COHERENT_DMA to provide user space a
    channel to notify KVM that guest memory specified by the memslot may be
    accessed by noncoherent DMA devices.
    
    KVM can start honoring guest memory type for this range of guest memory in
    platforms that do not always honoring guest PAT, e.g. in Intel's platform.
    
    Previously, the only way to let KVM be aware of noncoherent DMA devices
    is through KVM device for VFIO pass-through devices, in which case, KVM is
    notified that all guest memory may be accessed by noncoherent DMA devices.
    
    To avoid complication, flag KVM_MEM_NON_COHERENT_DMA is not allowed to be
    dynamically modified for a memslot.
    
    A KVM_CAP_USER_CONFIGURE_NONCOHERENT_DMA is added to let user space know if
    this new memslot flag is supported in KVM.
    
    Cc: Kevin Tian <kevin.tian@intel.com>
    Cc: Zhenyu Wang <zhenyuw@linux.intel.com>
    Tested-by: Yongwei Ma <yongwei.ma@intel.com>
    Signed-off-by: Yan Zhao <yan.y.zhao@intel.com>
    Signed-off-by: Dawn Han <dawnhan@google.com>
    (am from https://lore.kernel.org/all/20240105091346.24637-1-yan.y.zhao@intel.com/)
    
    Conflicts:
     include/uapi/linux/kvm.h
     virt/kvm/kvm_main.c
    
    BUG=b:335525888
    UPSTREAM-TASK=b:321924870
    TEST=run against ARCVM with change patch
    
    Change-Id: If47b890c45ed20179a578704f3b1fd186769a422
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5479187
    Tested-by: Dawn Han <dawnhan@google.com>
    Commit-Queue: Dawn Han <dawnhan@google.com>
    Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
    Kcr-patch: 1789f570afad312e920649a451b82f776314dd6f29d987b83ab34420.patch

--- virt/kvm/kvm_main.c
+++ virt/kvm/kvm_main.c
@@ -1738,6 +1738,8 @@ static int check_memory_region_flags(struct kvm *kvm,
 	    !(mem->flags & KVM_MEM_GUEST_MEMFD))
 		valid_flags |= KVM_MEM_READONLY;
 
+	valid_flags |= KVM_MEM_NON_COHERENT_DMA;
+
 	if (mem->flags & ~valid_flags)
 		return -EINVAL;
 
@@ -2208,7 +2210,8 @@ int __kvm_set_memory_region(struct kvm *kvm,
 			return -EINVAL;
 		if ((mem->userspace_addr != old->userspace_addr) ||
 		    (npages != old->npages) ||
-		    ((mem->flags ^ old->flags) & KVM_MEM_READONLY))
+		    ((mem->flags ^ old->flags) &
+		     (KVM_MEM_READONLY | KVM_MEM_NON_COHERENT_DMA)))
 			return -EINVAL;
 
 		if (base_gfn != old->base_gfn)
@@ -5094,6 +5097,7 @@ static int kvm_vm_ioctl_check_extension_generic(struct kvm *kvm, long arg)
 	case KVM_CAP_USER_MEMORY2:
 	case KVM_CAP_DESTROY_MEMORY_REGION_WORKS:
 	case KVM_CAP_JOIN_MEMORY_REGIONS_WORKS:
+	case KVM_CAP_USER_CONFIGURE_NONCOHERENT_DMA:
 	case KVM_CAP_INTERNAL_ERROR_DATA:
 #ifdef CONFIG_HAVE_KVM_MSI
 	case KVM_CAP_SIGNAL_MSI:
