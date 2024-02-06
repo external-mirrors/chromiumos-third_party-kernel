39c73dcc47eac5deadd9b8bc99d5bda13efabf7d
Bartłomiej Bujak <bbujak@google.com>
2024-02-06 15:23:33 +0000

    FIXUP: CHROMIUM: drm/img-rogue: Add 1.17 IMG PowerVR Rogue driver
    drm: Remove locking for legacy ioctls and DRM_UNLOCKED
    
    Change-Id: Ide0768aa9e8614911d54db26239f074d6a7d819a
    Signed-off-by: Bartłomiej Bujak <bbujak@google.com>
    Kcr-depends-on-sha: 2798ffcc1d6a788b5769b1fbcf0750dfc06ae98a
    Kcr-patch: fixups/FIXUP: CHROMIUM: drm img-rogue: Add 1.17 IMG PowerVR Rogue driver2.patch

--- drivers/gpu/drm/img-rogue/1.17/pvr_drm.c
+++ drivers/gpu/drm/img-rogue/1.17/pvr_drm.c
@@ -266,18 +266,18 @@ static void pvr_drm_release(struct drm_device *ddev, struct drm_file *dfile)
  */
 static struct drm_ioctl_desc pvr_drm_ioctls[] = {
 	DRM_IOCTL_DEF_DRV(PVR_SRVKM_CMD, PVRSRV_BridgeDispatchKM,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(PVR_SRVKM_INIT, drm_pvr_srvkm_init,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 #if defined(SUPPORT_NATIVE_FENCE_SYNC) && !defined(USE_PVRSYNC_DEVNODE)
 	DRM_IOCTL_DEF_DRV(PVR_SYNC_RENAME_CMD, pvr_sync_rename_ioctl,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(PVR_SYNC_FORCE_SW_ONLY_CMD, pvr_sync_force_sw_only_ioctl,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(PVR_SW_SYNC_CREATE_FENCE_CMD, pvr_sw_sync_create_fence_ioctl,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 	DRM_IOCTL_DEF_DRV(PVR_SW_SYNC_INC_CMD, pvr_sw_sync_inc_ioctl,
-			  DRM_RENDER_ALLOW | DRM_UNLOCKED),
+			  DRM_RENDER_ALLOW),
 #endif
 };
 
