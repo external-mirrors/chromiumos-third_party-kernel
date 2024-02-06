7d10f5ba80276cd128d1c7dd5ac50d1b00d0623b
Bartłomiej Bujak <bbujak@google.com>
2024-02-06 15:29:56 +0000

    FIXUP: CHROMIUM: media: add camx drivers
    of: Stop circularly including of_device.h and of_platform.h
    
    Change-Id: Id9e99c5956b5bdcda747a86d2372bf6127e5d51e
    Signed-off-by: Bartłomiej Bujak <bbujak@google.com>
    Kcr-depends-on-sha: ef175b29a242fea98f467f008237484b03c94834
    Kcr-patch: fixups/FIXUP: CHROMIUM: media: add camx drivers - Stop circularly including of_device.h and of_platform.h.patch

--- drivers/media/platform/camx/cam_icp/icp_hw/ipe_hw/ipe_dev.c
+++ drivers/media/platform/camx/cam_icp/icp_hw/ipe_hw/ipe_dev.c
@@ -15,6 +15,7 @@
 #include <linux/slab.h>
 #include <linux/mod_devicetable.h>
 #include <linux/of_device.h>
+#include <linux/of_platform.h>
 #include <linux/timer.h>
 #include "ipe_core.h"
 #include "ipe_soc.h"
