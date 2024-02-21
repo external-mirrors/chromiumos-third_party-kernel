3d96991ddf04a261fd005f15459f1149fe9e3efc
David Stevens <stevensd@chromium.org>
2024-02-21 11:43:00 +0900

    BACKPORT: FROMLIST: KVM: x86/mmu: Handle non-refcounted pages
    Handle non-refcounted pages in __kvm_faultin_pfn. This allows the
    host to map memory into the guest that is backed by non-refcounted
    struct pages - for example, the tail pages of higher order non-compound
    pages allocated by the amdgpu driver via ttm_pool_alloc_page.
    
    Signed-off-by: David Stevens <stevensd@chromium.org>
    (am from https://patchwork.kernel.org/patch/13576450/)
    (also found at https://lore.kernel.org/r/20240229025759.1187910-9-stevensd@google.com)
    
    BUG=b:328351865
    UPSTREAM-TASK=b:265081912
    TEST=tast run zork arc.Boot.vm
    TEST=No panic on lazor after logging in 5 times with kernel
    TEST=build with USE="debug lockdebug kcov"
    
    Change-Id: Id1a316854775777bf58347618f4bd935d2497316
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5359867
    Tested-by: Stephen Boyd <swboyd@chromium.org>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Stephen Boyd <swboyd@chromium.org>
    Tested-by: David Stevens <stevensd@chromium.org>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>
    Kcr-patch: 15b2d96d336f42090d5e1237055315cc4cd9b1ab3e0da6b64092890c.patch
    
    [rebase 612:
     Replaced kvm_release_pfn_clean with kvm_set_page_accessed in
     kvm_faultin_pfn. See https://lore.kernel.org/all/CAD=HUj6gjqthaenpZUr2Lci3WJW86OWe559YrDWiUSVfZBNHiQ@mail.gmail.com/ for details.
    ]
    
    Change-Id: Ie8672e9409488a14298a35261a9a19012bfd7b62

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -2923,6 +2923,11 @@ static int mmu_set_spte(struct kvm_vcpu *vcpu, struct kvm_memory_slot *slot,
 	bool host_writable = !fault || fault->map_writable;
 	bool prefetch = !fault || fault->prefetch;
 	bool write_fault = fault && fault->write;
+	/*
+	 * Prefetching uses gfn_to_page_many_atomic, which never gets
+	 * non-refcounted pages.
+	 */
+	bool is_refcounted = !fault || !!fault->accessed_page;
 
 	if (unlikely(is_noslot_pfn(pfn))) {
 		vcpu->stat.pf_mmio_spte_created++;
@@ -2950,7 +2955,7 @@ static int mmu_set_spte(struct kvm_vcpu *vcpu, struct kvm_memory_slot *slot,
 	}
 
 	wrprot = make_spte(vcpu, sp, slot, pte_access, gfn, pfn, *sptep, prefetch,
-			   true, host_writable, true, &spte);
+			   true, host_writable, is_refcounted, &spte);
 
 	if (*sptep == spte) {
 		ret = RET_PF_SPURIOUS;
@@ -4411,10 +4416,10 @@ static int __kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	struct kvm_follow_pfn kfp = {
 		.slot = slot,
 		.gfn = fault->gfn,
-		.flags = FOLL_GET | (fault->write ? FOLL_WRITE : 0),
+		.flags = fault->write ? FOLL_WRITE : 0,
 		.try_map_writable = true,
 		.guarded_by_mmu_notifier = true,
-		.allow_non_refcounted_struct_page = false,
+		.allow_non_refcounted_struct_page = shadow_refcounted_mask,
 	};
 
 	if (fault->is_private)
@@ -4462,6 +4467,7 @@ static int __kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 success:
 	fault->hva = kfp.hva;
 	fault->map_writable = kfp.writable;
+	fault->accessed_page = kfp.refcounted_page;
 	return RET_PF_CONTINUE;
 }
 
@@ -4567,7 +4573,7 @@ static int kvm_faultin_pfn(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault,
 	 * mmu_lock is acquired.
 	 */
 	if (mmu_invalidate_retry_gfn_unsafe(vcpu->kvm, fault->mmu_seq, fault->gfn)) {
-		kvm_release_pfn_clean(fault->pfn);
+		kvm_set_page_accessed(fault->accessed_page);
 		return RET_PF_RETRY;
 	}
 
@@ -4643,8 +4649,8 @@ static int direct_page_fault(struct kvm_vcpu *vcpu, struct kvm_page_fault *fault
 	r = direct_map(vcpu, fault);
 
 out_unlock:
+	kvm_set_page_accessed(fault->accessed_page);
 	write_unlock(&vcpu->kvm->mmu_lock);
-	kvm_release_pfn_clean(fault->pfn);
 	return r;
 }
 
@@ -4730,8 +4736,8 @@ static int kvm_tdp_mmu_page_fault(struct kvm_vcpu *vcpu,
 	r = kvm_tdp_mmu_map(vcpu, fault);
 
 out_unlock:
+	kvm_set_page_accessed(fault->accessed_page);
 	read_unlock(&vcpu->kvm->mmu_lock);
-	kvm_release_pfn_clean(fault->pfn);
 	return r;
 }
 #endif
