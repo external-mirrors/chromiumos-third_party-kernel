2694c096bdff599dceea7b03d5e17c1d35361793
Haikun Zhou <zhouhaikun5@huaqin.corp-partner.google.com>
2024-05-22 19:39:24 +0800

    FROMGIT: drm/panel-edp: Add CMN N116BCJ-EAK
    Add support for the CMN N116BCJ-EAK, place the raw EDID here for
    subsequent reference.
    00 ff ff ff ff ff ff 00 0d ae 60 11 00 00 00 00
    04 22 01 04 95 1a 0e 78 02 67 75 98 59 53 90 27
    1c 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 da 1d 56 e2 50 00 20 30 30 20
    a6 00 00 90 10 00 00 18 00 00 00 fe 00 4e 31 31
    36 42 43 4a 2d 45 41 4b 0a 20 00 00 00 fe 00 43
    4d 4e 0a 20 20 20 20 20 20 20 20 20 00 00 00 fe
    00 4e 31 31 36 42 43 4a 2d 45 41 4b 0a 20 00 98
    
    Signed-off-by: Haikun Zhou <zhouhaikun5@huaqin.corp-partner.google.com>
    Reviewed-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240522113924.1261683-1-zhouhaikun5@huaqin.corp-partner.google.com
    (cherry picked from commit 7acacca1b157fcb258cfd781603425f73bc7370b
     https://anongit.freedesktop.org/git/drm/drm-misc.git drm-misc-next)
    
    BUG=b:342285085
    TEST=emerge-corsola sys-kernel/chromeos-kernel-5_15
    
    Change-Id: Id8e56472511df9bd56cb75704349be8109dfb709
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5562223
    Reviewed-by: Sean Paul <sean@poorly.run>
    Reviewed-by: Wentao Qin <qinwentao@huaqin.corp-partner.google.com>
    Tested-by: Haikun Zhou <zhouhaikun5@huaqin.corp-partner.google.com>
    Reviewed-by: Hsin-Yi Wang <hsinyi@chromium.org>
    Commit-Queue: Hsin-Yi Wang <hsinyi@chromium.org>
    (cherry picked from commit c3d4a07362ef3d21d5fe324d00e9034660d89726)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5568898
    Commit-Queue: Knox Chiou <knoxchiou@chromium.org>
    Reviewed-by: Knox Chiou <knoxchiou@chromium.org>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -1942,6 +1942,7 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x1154, &delay_200_500_e80_d50, "N116BCA-EA2"),
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x1157, &delay_200_500_e80_d50, "N116BGE-EA2"),
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x115b, &delay_200_500_e80_d50, "N116BCN-EB1"),
+	EDP_PANEL_ENTRY('C', 'M', 'N', 0x1160, &delay_200_500_e80_d50, "N116BCJ-EAK"),
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x1247, &delay_200_500_e80_d50, "N120ACA-EA1"),
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x142b, &delay_200_500_e80_d50, "N140HCA-EAC"),
 	EDP_PANEL_ENTRY('C', 'M', 'N', 0x144f, &delay_200_500_e80_d50, "N140HGA-EA1"),
