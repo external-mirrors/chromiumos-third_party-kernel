3b9a55dfb523cd11e24acf138902c8eaa4d619ff
Chris Lu <chris.lu@mediatek.corp-partner.google.com>
2024-09-25 11:03:17 +0800

    BACKPORT: FROMGIT: Bluetooth: btusb: mediatek: add callback function in btusb_disconnect
    Add disconnect callback function in btusb_disconnect which is reserved
    for vendor specific usage before deregister hci in btusb_disconnect.
    
    Signed-off-by: Chris Lu <chris.lu@mediatek.com>
    Signed-off-by: Luiz Augusto von Dentz <luiz.von.dentz@intel.com>
    (cherry picked from commit bcfc09e8a15d34c4d399a0ba5f88d698049b0687
     https://git.kernel.org/pub/scm/linux/kernel/git/bluetooth/bluetooth-next.git master)
    
    BUG=b:308395932, b:364162503
    TEST=build pass bluetooth basic function pass on Brya
    
    Backport Notes:
    MediaTek BT reset flow refactor in the latest Kerenl.
    
    Fix Conflict:
    drivers/bluetooth/btusb.c
    
    Change-Id: Icc92abf202844f6858fe025c119a1ac579e0004d
    Signed-off-by: Chris Lu <chris.lu@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5890062
    Commit-Queue: Yun-hao Chung <howardchung@chromium.org>
    Reviewed-by: Yun-hao Chung <howardchung@chromium.org>
    Tested-by: Yun-hao Chung <howardchung@chromium.org>

--- drivers/bluetooth/btusb.c
+++ drivers/bluetooth/btusb.c
@@ -845,6 +845,7 @@ struct btusb_data {
 
 	int (*suspend)(struct hci_dev *hdev);
 	int (*resume)(struct hci_dev *hdev);
+	int (*disconnect)(struct hci_dev *hdev);
 
 	int oob_wake_irq;   /* irq for out-of-band wake-on-bt */
 
@@ -4044,6 +4045,9 @@ static void btusb_disconnect(struct usb_interface *intf)
 
 	if (data->diag)
 		usb_set_intfdata(data->diag, NULL);
+
+	if (data->disconnect)
+		data->disconnect(hdev);
 
 	hci_unregister_dev(hdev);
 
