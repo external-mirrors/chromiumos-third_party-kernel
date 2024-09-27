551173e8d5b1d13123ba629389cf0fb1e7c46840
Henry Barnor <hbarnor@chromium.org>
2024-09-27 16:13:54 -0700

    CHROMIUM: HID: multitouch: Scope mt_reset actions to haptic devices
    To handle haptic device reset, mt_reset was added to PM functions.
    In kernel 6.6, the calls to mt_reset seems to negatively impact an
    increasing number of devices across different manufacturers. To avoid
    continuously adding devices to the quirk: SKIP_RESET, we are limiting
    the reset actions to only haptic devices. This is a temporary workaround
    whiles we come up with a long term robust fix that will be part
    of the effort to upstream support for haptic touchpads.
    
    BUG=b:352329398
    TEST=Build and test kernel on affected devices
    UPSTREAM-TASK=b:298170707
    
    Change-Id: I74bc44408b7b44a199e0a50b60fe444a9da71329
    Signed-off-by: Henry Barnor <hbarnor@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5895129
    
    Origin-6-6: 0a81a517ff8f3193026b65358eb19796b2e183c6
    Squashed: https://crrev.com/5f999be40315f58ee809b52e455780d14c7b3c46 FIXUP: CHROMIUM: HID: multitouch: Scope mt_reset actions to haptic devices
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- drivers/hid/hid-multitouch.c
+++ drivers/hid/hid-multitouch.c
@@ -2142,11 +2142,11 @@ static int mt_reset(struct hid_device *hdev)
 	if (!td)
 		return 0;
 
-	mt_release_contacts(hdev);
-	mt_set_modes(hdev, HID_LATENCY_NORMAL, true, true);
-
-	if (td->is_haptic_touchpad)
+	if (td->is_haptic_touchpad) {
+		mt_release_contacts(hdev);
+		mt_set_modes(hdev, HID_LATENCY_NORMAL, true, true);
 		hid_haptic_reset(hdev, td->haptic);
+	}
 
 	return 0;
 }
