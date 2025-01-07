de8055d2089eb00f8c5bac76f6d66346f795692b
Johannes Berg <johannes.berg@intel.com>
2025-01-07 11:01:25 +0100

    CHROMIUM: iwl7000: backports/pcie: fixes for PCI mappings
    To handle all kernel versions including 6.13, we need to
    adjust the backports here appropriately. Do that.
    
    Change-Id: I51a30fde53792ff8f0e84f407fc424040a27775e
    Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    iwl7000-tree: 458f2d7e6f1f2b6538cd56ef508f298f70cf1bdb

--- drivers/net/wireless/iwl7000/hdrs/backports.h
+++ drivers/net/wireless/iwl7000/hdrs/backports.h
@@ -27,6 +27,22 @@ static inline bool iwl7000_wiphy_ext_feature_isset(struct wiphy *wiphy,
 #define wiphy_ext_feature_set iwl7000_wiphy_ext_feature_set
 #define wiphy_ext_feature_isset iwl7000_wiphy_ext_feature_isset
 
+static inline int pcim_request_all_regions(struct pci_dev *pdev, const char *name)
+{
+	/* NOTE: this only works with pcim_enable_device() on older kernels */
+	int mask = 0;
+
+	for (int i = 0; i < PCI_STD_NUM_BARS; i++) {
+		if (!pci_resource_start(pdev, i))
+			continue;
+		if (!pci_resource_len(pdev, i))
+			continue;
+		mask |= BIT(i);
+	}
+
+	return pci_request_selected_regions(pdev, mask, name);
+}
+
 
 struct cfg80211_mlo_reconf_done_data {
 	const u8 *buf;
