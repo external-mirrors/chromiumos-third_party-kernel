406c5f128c61e2776fc69082030750f3af82cd72
Yedidya Benshimol <yedidya.ben.shimol@intel.com>
2025-03-13 10:35:07 +0200

    CHROMIUM: iwl7000: mld: reduece scope for uninitialized variable
    After resuming from D3, keeping the connection or disconnecting
    isn't relevant for the case of netdetect.
    Reduece the scope of the keep_connection indicator to wowlan only.
    
    Signed-off-by: Yedidya Benshimol <yedidya.ben.shimol@intel.com>
    Change-Id: I69e8f194997eb3a20e40d27fdc31002d5753d905
    iwl7000-tree: ecd95b8b82b4d222918ef79f556caa0510e71c7a

--- drivers/net/wireless/iwl7000/iwlwifi/mld/d3.c
+++ drivers/net/wireless/iwl7000/iwlwifi/mld/d3.c
@@ -1896,7 +1896,6 @@ int iwl_mld_wowlan_resume(struct iwl_mld *mld)
 	int link_id;
 	int ret;
 	bool fw_err = false;
-	bool keep_connection;
 
 	lockdep_assert_wiphy(mld->wiphy);
 
@@ -1966,7 +1965,7 @@ int iwl_mld_wowlan_resume(struct iwl_mld *mld)
 		iwl_mld_process_netdetect_res(mld, bss_vif, &resume_data);
 		mld->netdetect = false;
 	} else {
-		keep_connection =
+		bool keep_connection =
 			iwl_mld_process_wowlan_status(mld, bss_vif,
 						      resume_data.wowlan_status);
 
@@ -1974,10 +1973,9 @@ int iwl_mld_wowlan_resume(struct iwl_mld *mld)
 		if (keep_connection)
 			iwl_mld_unblock_emlsr(mld, bss_vif,
 					      IWL_MLD_EMLSR_BLOCKED_WOWLAN);
+		else
+			ieee80211_resume_disconnect(bss_vif);
 	}
-
-	if (!mld->netdetect && !keep_connection)
-		ieee80211_resume_disconnect(bss_vif);
 
 	goto out;
 
