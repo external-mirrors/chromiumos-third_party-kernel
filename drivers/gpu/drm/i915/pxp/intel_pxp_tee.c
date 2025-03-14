4ff1d739a08e3ada31ae1d8b5e9295387fbf0476
FNU VISHWANATHA <fnu.vishwanatha@intel.corp-partner.google.com>
2025-03-14 13:09:39 -0700

    CHROMIUM: drm/i915/pxp: handle interrupted PXP TEE messages
    If a signal occurs during PXP TEE messages, MEI PXP exits and doesn't
    wait for the response. The send still went through though and a response
    will come causing the next PXP TEE message to send its own message but
    then receive the response from the previously interrupted message and we
    end up with all subsequent messages getting out of sync.
    
    Flag whenever a message is interrupted so on the next message we can
    drop that message before starting a new send/recv.
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    Reviewed-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Signed-off-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Acked-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    
    BUG=b:396348727
    TEST=i915 driver loaded successfully
    
    Change-Id: Ide3679d58ca9f53d4e8f9d08c9df492dcd7b1f49
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5231527
    Tested-by: Altaf Basha <altaf.basha@intel.com>
    Reviewed-by: Juston Li <justonli@google.com>
    Signed-off-by: Fnu Vishwanatha<fnu.vishwanatha@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6301110
    Commit-Queue: Marek Maślanka <mmaslanka@google.com>
    Tested-by: FNU VISHWANATHA <fnu.vishwanatha@intel.com>
    Reviewed-by: Kornel Dulęba <korneld@google.com>

--- drivers/gpu/drm/i915/pxp/intel_pxp_tee.c
+++ drivers/gpu/drm/i915/pxp/intel_pxp_tee.c
@@ -62,6 +62,7 @@ static int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 {
 	struct drm_i915_private *i915 = pxp->ctrl_gt->i915;
 	struct i915_pxp_component *pxp_component = pxp->pxp_component;
+	u8 tmp_drop_buf[64];
 	int ret = 0;
 
 	mutex_lock(&pxp->tee_mutex);
@@ -75,9 +76,22 @@ static int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 		goto unlock;
 	}
 
+	if (pxp->mei_pxp_last_msg_interrupted) {
+		/* read and drop data from the previous iteration */
+		ret = pxp_component->ops->recv(pxp_component->tee_dev, &tmp_drop_buf, 64,
+					       PXP_TRANSPORT_TIMEOUT_MS);
+		if (ret == -EINTR)
+			goto unlock;
+
+		pxp->mei_pxp_last_msg_interrupted = false;
+	}
+
 	ret = pxp_component->ops->send(pxp_component->tee_dev, msg_in, msg_in_size,
 				       PXP_TRANSPORT_TIMEOUT_MS);
 	if (ret) {
+		/* flag on next msg to drop interrupted msg */
+		if (ret == -EINTR)
+			pxp->mei_pxp_last_msg_interrupted = true;
 		drm_err(&i915->drm, "Failed to send PXP TEE message\n");
 		goto unlock;
 	}
@@ -85,6 +99,9 @@ static int intel_pxp_tee_io_message(struct intel_pxp *pxp,
 	ret = pxp_component->ops->recv(pxp_component->tee_dev, msg_out, msg_out_max_size,
 				       PXP_TRANSPORT_TIMEOUT_MS);
 	if (ret < 0) {
+		/* flag on next msg to drop interrupted msg */
+		if (ret == -EINTR)
+			pxp->mei_pxp_last_msg_interrupted = true;
 		drm_err(&i915->drm, "Failed to receive PXP TEE message\n");
 		goto unlock;
 	}
