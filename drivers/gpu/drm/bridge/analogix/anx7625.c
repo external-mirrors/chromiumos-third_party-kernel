4df016999d4f12abfb8ff74242a1e3a09e51a59b
Fei Shao <fshao@chromium.org>
2025-03-28 03:43:20 -0700

    Revert "CHROMIUM: drm/bridge: anx7625: Fix not display after S4/S5 resume back"
    This reverts commit 41072194329f049939f6bcbfe2e56a719d7b3c92.
    
    Reason for revert: land fromlist
    
    Original change's description:
    > CHROMIUM: drm/bridge: anx7625: Fix not display after S4/S5 resume back
    >
    > When dock with monitor attached to OS, HPD will always high. When kernel
    > boot up, anx7625 driver will probe device, and power on. Sometimes
    > firmware will not report HPD change interrupt as HPD keep high before
    > power on. Then driver should poll the HPD event after received status
    > change interrupt.
    >
    > NOTE: we land this on Rauru factory branch only to unblock factory.
    >       Revert and reapply the official fix after we have a conclusion
    >       in the original CL in main branch.
    >
    > TEST=tested on navi
    > BUG=b:397133556
    > BUG= b:388678903, b:378586053, b:405324559, b:400522382
    > UPSTREAM-TASK=b:405836306
    >
    > Fixes: da7f49e653bb6 ("CHROMIUM: drm/bridge: anx7625: Add DSC feature support")
    >
    > Change-Id: I043c8bfeaece6665b50f08d26dbe662a2833aba7
    > Signed-off-by: Xin Ji <xji@analogix.corp-partner.google.com>
    > Signed-off-by: Fei Shao <fshao@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6389896
    > Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    
    BUG=b:397133556
    BUG=b:388678903, b:378586053, b:405324559, b:400522382
    
    Change-Id: I9c0df3f70552ccda9fd8361797b119c158e706e7
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/6407337
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>

--- drivers/gpu/drm/bridge/analogix/anx7625.c
+++ drivers/gpu/drm/bridge/analogix/anx7625.c
@@ -1661,13 +1661,13 @@ static int anx7625_read_hpd_status_p0(struct anx7625_data *ctx)
 }
 
 static int _anx7625_hpd_polling(struct anx7625_data *ctx,
-				unsigned long wait_us, bool force_polling)
+				unsigned long wait_us)
 {
 	int ret, val;
 	struct device *dev = ctx->dev;
 
 	/* Interrupt mode, no need poll HPD status, just return */
-	if (ctx->pdata.intp_irq && !force_polling)
+	if (ctx->pdata.intp_irq)
 		return 0;
 
 	ret = readx_poll_timeout(anx7625_read_hpd_status_p0,
@@ -1702,7 +1702,7 @@ static int anx7625_wait_hpd_asserted(struct drm_dp_aux *aux,
 	int ret;
 
 	pm_runtime_get_sync(dev);
-	ret = _anx7625_hpd_polling(ctx, wait_us, false);
+	ret = _anx7625_hpd_polling(ctx, wait_us);
 	pm_runtime_mark_last_busy(dev);
 	pm_runtime_put_autosuspend(dev);
 
@@ -1775,11 +1775,8 @@ static int anx7625_hpd_change_detect(struct anx7625_data *ctx)
 		return status;
 	}
 
-	if (!(intr_vector & HPD_STATUS_CHANGE)) {
-		_anx7625_hpd_polling(ctx, 1000*100, true);
-		/* No need to raise up irq event */
+	if (!(intr_vector & HPD_STATUS_CHANGE))
 		return -ENOENT;
-	}
 
 	status = anx7625_reg_read(ctx, ctx->i2c.rx_p0_client,
 				  SYSTEM_STSTUS);
@@ -1992,7 +1989,7 @@ static struct edid *anx7625_get_edid(struct anx7625_data *ctx)
 	}
 
 	pm_runtime_get_sync(dev);
-	_anx7625_hpd_polling(ctx, 5000 * 100, false);
+	_anx7625_hpd_polling(ctx, 5000 * 100);
 	edid_num = sp_tx_edid_read(ctx, p_edid->edid_raw_data);
 	pm_runtime_put_sync(dev);
 
@@ -2870,7 +2867,7 @@ static void anx7625_bridge_atomic_enable(struct drm_bridge *bridge,
 	ctx->connector = connector;
 
 	pm_runtime_get_sync(dev);
-	_anx7625_hpd_polling(ctx, 5000 * 100, false);
+	_anx7625_hpd_polling(ctx, 5000 * 100);
 
 	anx7625_dp_start(ctx);
 	/*
@@ -3349,7 +3346,7 @@ static int anx7625_i2c_probe(struct i2c_client *client)
 	if (!platform->pdata.low_power_mode) {
 		anx7625_disable_pd_protocol(platform);
 		pm_runtime_get_sync(dev);
-		_anx7625_hpd_polling(platform, 5000 * 100, false);
+		_anx7625_hpd_polling(platform, 5000 * 100);
 	}
 
 	/* Add work function */
