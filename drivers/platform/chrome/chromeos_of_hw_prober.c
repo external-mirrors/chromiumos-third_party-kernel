3d1c7ccdbfbe7ae22e1090ca34b9d9636eba9d11
Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
2025-11-06 15:45:09 +0800

    CHROMIUM: platform/chrome: of_hw_prober: Support touchscreen and touchpad probing on Grogu
    The MT8189 Grogu Chromebook is built with one of two possible
    touchscreens and trackpad. Let the prober probe for them.
    
    BUG=b:445833669
    TEST=1. cros workon --board skywalker start chromeos-kernel-6_6
         2. emerge-skywalker chromeos-kernel-6_6
    UPSTREAM-TASK=b:452484898
    
    Change-Id: I92a0b2f5ffc68341f7bc7094c117e57955fa927c
    Signed-off-by: Daniel Peng <Daniel_Peng@pegatron.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7122889
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-by: Shawn Ku <shawnku@chromium.org>
    Tested-by: Daniel Peng <daniel_peng@pegatron.corp-partner.google.com>
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Commit-Queue: Pin-yen Lin <treapking@chromium.org>
    (cherry picked from commit 06c60054ee2425f7e03e20204d5291bbd0e03ce0)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7144605
    Auto-Submit: Pin-yen Lin <treapking@chromium.org>
    Commit-Queue: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Tested-by: Pin-yen Lin <treapking@chromium.org>

--- drivers/platform/chrome/chromeos_of_hw_prober.c
+++ drivers/platform/chrome/chromeos_of_hw_prober.c
@@ -132,6 +132,17 @@ static const struct chromeos_i2c_probe_data chromeos_i2c_probe_tarkin_touchscree
 	},
 };
 
+static const struct chromeos_i2c_probe_data chromeos_i2c_probe_grogu_touchscreen = {
+	.cfg = &chromeos_i2c_probe_simple_touchscreen_cfg,
+	.opts = &(const struct i2c_of_probe_simple_opts) {
+		.res_node_compatible = "elan,ekth6a12nay",
+		.supply_name = "vcc33",
+		.gpio_name = "reset",
+		.post_power_on_delay_ms = 10,
+		.post_gpio_config_delay_ms = 100,
+	},
+};
+
 static const struct hw_prober_entry hw_prober_platforms[] = {
 	{
 		.compatible = "google,hana",
@@ -196,6 +207,14 @@ static const struct hw_prober_entry hw_prober_platforms[] = {
 		.data = &chromeos_i2c_probe_tarkin_touchscreen,
 	}, {
 		.compatible = "google,tarkin",
+		.prober = chromeos_i2c_component_prober,
+		.data = &chromeos_i2c_probe_dumb_trackpad,
+	}, {
+		.compatible = "google,grogu",
+		.prober = chromeos_i2c_component_prober,
+		.data = &chromeos_i2c_probe_grogu_touchscreen,
+	}, {
+		.compatible = "google,grogu",
 		.prober = chromeos_i2c_component_prober,
 		.data = &chromeos_i2c_probe_dumb_trackpad,
 	}
