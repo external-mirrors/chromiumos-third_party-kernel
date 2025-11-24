c656768e3ede2d2822c62e682b6fdf884df71dcb
xiandong.wang <xiandong.wang@mediatek.corp-partner.google.com>
2025-11-24 20:22:50 +0800

    CHROMIUM: mediatek/drm: Refactor mtk_crtc structure definition
    Refactored the mtk_crtc structure definition by moving it from
    mtk_crtc.c to mtk_crtc.h for better modularity and maintainability.
    This change ensures the structure is accessible across multiple files
    and aligns with standard practices for header file usage.
    
    - Removed redundant includes in mtk_crtc.c
    - Added necessary includes in mtk_crtc.h
    - Updated comments and structure definitions for clarity
    
    BUG=b:444097386
    UPSTREAM-TASK=b:396344738
    TEST=build pass & boot pass & panel show normal both with one and two panel
    
    Change-Id: I3d50c5192f2ef2ce0bdcaa95f44d4796aad933d5
    Signed-off-by: Xiandong Wang <xiandong.wang@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7198601
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Tested-by: Pin-yen Lin <treapking@chromium.org>
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_crtc.c
+++ drivers/gpu/drm/mediatek/mtk_crtc.c
@@ -23,12 +23,10 @@
 #include <drm/display/drm_dsc_helper.h>
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
-#include <drm/drm_vblank_work.h>
 
 #include "mtk_crtc.h"
 #include "mtk_ddp_comp.h"
 #include "mtk_disp_pmqos.h"
-#include "mtk_drm_drv.h"
 #include "mtk_gem.h"
 #include "mtk_plane.h"
 
@@ -40,74 +38,6 @@ struct mtk_crtc_qos_ctx {
 	unsigned int last_plane_srt_req[MAX_PLANE];
 	unsigned int plane_srt_req[MAX_PLANE];
 	unsigned int plane_channel_id[MAX_PLANE];
-};
-
-/*
- * struct mtk_crtc - MediaTek specific crtc structure.
- * @base: crtc object.
- * @enabled: records whether crtc_enable succeeded
- * @planes: array of 4 drm_plane structures, one for each overlay plane
- * @pending_planes: whether any plane has pending changes to be applied
- * @mmsys_dev: pointer to the mmsys device for configuration registers
- * @mutex: handle to one of the ten disp_mutex streams
- * @ddp_comp_nr: number of components in ddp_comp
- * @ddp_comp: array of pointers the mtk_ddp_comp structures used by this crtc
- *
- * TODO: Needs update: this header is missing a bunch of member descriptions.
- */
-struct mtk_crtc {
-	struct drm_crtc			base;
-	bool				enabled;
-
-	bool				pending_needs_vblank;
-	struct drm_pending_vblank_event	*event;
-
-	struct drm_plane		*planes;
-	unsigned int			layer_nr;
-	bool				pending_planes;
-	bool				pending_async_planes;
-
-#if IS_REACHABLE(CONFIG_MTK_CMDQ)
-	struct cmdq_client		cmdq_client;
-	struct cmdq_pkt			cmdq_handle;
-	u32				cmdq_event;
-	u32				cmdq_vblank_cnt;
-	wait_queue_head_t		cb_blocking_queue;
-	struct task_struct		*cmdq_done_task;
-	atomic_t			cmdq_done;
-
-	struct cmdq_client		sec_cmdq_client;
-	bool				sec_cmdq_working;
-	wait_queue_head_t		sec_cb_blocking_queue;
-#endif
-
-	struct device			*mmsys_dev[MAX_MMSYS];
-	struct device			*dma_dev;
-	struct device			*vdisp_ao_dev;
-	struct device			*dpc_dev;
-	struct device			*pmqos_dev;
-	struct mtk_mutex		*mutex[MAX_MMSYS];
-	unsigned int			ddp_comp_nr;
-	struct mtk_ddp_comp		**ddp_comp;
-	enum mtk_drm_mmsys		*ddp_comp_sys;
-	bool				exist[MAX_MMSYS];
-	unsigned int			num_conn_routes;
-	const struct mtk_drm_route	*conn_routes;
-	enum mtk_drm_mmsys		conn_routes_sys;
-
-	/* lock for display hardware access */
-	struct mutex			hw_lock;
-	bool				config_updating;
-	/* lock for config_updating to cmd buffer */
-	spinlock_t			config_lock;
-
-	/* support crc */
-	struct mtk_ddp_comp		*crc_provider;
-	struct drm_vblank_work		crc_work;
-
-	bool				sec_on;
-	/* QoS setting*/
-	struct mtk_crtc_qos_ctx		*qos_ctx;
 };
 
 struct mtk_crtc_state {
--- drivers/gpu/drm/mediatek/mtk_crtc.h
+++ drivers/gpu/drm/mediatek/mtk_crtc.h
@@ -7,6 +7,7 @@
 #define MTK_CRTC_H
 
 #include <drm/drm_crtc.h>
+#include <drm/drm_vblank_work.h>
 #include "mtk_ddp_comp.h"
 #include "mtk_drm_drv.h"
 #include "mtk_plane.h"
@@ -45,6 +46,74 @@ struct mtk_crtc_crc {
 enum DISP_ATF_CMD {
 	DISP_ATF_CMD_CONFIG_DISP_CONFIG, /* For display master to disable secure */
 	DISP_ATF_CMD_COUNT,
+};
+
+/*
+ * struct mtk_crtc - MediaTek specific crtc structure.
+ * @base: crtc object.
+ * @enabled: records whether crtc_enable succeeded
+ * @planes: array of 4 drm_plane structures, one for each overlay plane
+ * @pending_planes: whether any plane has pending changes to be applied
+ * @mmsys_dev: pointer to the mmsys device for configuration registers
+ * @mutex: handle to one of the ten disp_mutex streams
+ * @ddp_comp_nr: number of components in ddp_comp
+ * @ddp_comp: array of pointers the mtk_ddp_comp structures used by this crtc
+ *
+ * TODO: Needs update: this header is missing a bunch of member descriptions.
+ */
+struct mtk_crtc {
+	struct drm_crtc			base;
+	bool				enabled;
+
+	bool				pending_needs_vblank;
+	struct drm_pending_vblank_event	*event;
+
+	struct drm_plane		*planes;
+	unsigned int			layer_nr;
+	bool				pending_planes;
+	bool				pending_async_planes;
+
+#if IS_REACHABLE(CONFIG_MTK_CMDQ)
+	struct cmdq_client		cmdq_client;
+	struct cmdq_pkt			cmdq_handle;
+	u32				cmdq_event;
+	u32				cmdq_vblank_cnt;
+	wait_queue_head_t		cb_blocking_queue;
+	struct task_struct		*cmdq_done_task;
+	atomic_t			cmdq_done;
+
+	struct cmdq_client		sec_cmdq_client;
+	bool				sec_cmdq_working;
+	wait_queue_head_t		sec_cb_blocking_queue;
+#endif
+
+	struct device			*mmsys_dev[MAX_MMSYS];
+	struct device			*dma_dev;
+	struct device			*vdisp_ao_dev;
+	struct device			*dpc_dev;
+	struct device			*pmqos_dev;
+	struct mtk_mutex		*mutex[MAX_MMSYS];
+	unsigned int			ddp_comp_nr;
+	struct mtk_ddp_comp		**ddp_comp;
+	enum mtk_drm_mmsys		*ddp_comp_sys;
+	bool				exist[MAX_MMSYS];
+	unsigned int			num_conn_routes;
+	const struct mtk_drm_route	*conn_routes;
+	enum mtk_drm_mmsys		conn_routes_sys;
+
+	/* lock for display hardware access */
+	struct mutex			hw_lock;
+	bool				config_updating;
+	/* lock for config_updating to cmd buffer */
+	spinlock_t			config_lock;
+
+	/* support crc */
+	struct mtk_ddp_comp		*crc_provider;
+	struct drm_vblank_work		crc_work;
+
+	bool				sec_on;
+	/* QoS setting*/
+	struct mtk_crtc_qos_ctx		*qos_ctx;
 };
 
 void mtk_crtc_check_fast_modeset(struct drm_crtc_state *old_crtc_state,
