cd8c2fe3615b875d53ab3c6bcf478f5176f9e578
Terry Hsiao <terry_hsiao@compal.corp-partner.google.com>
2026-05-12 13:54:45 +0800

    FROMGIT: drm/panel-edp: Add support for multiple AUO panels
    The raw EDIDs for each panel:
    
    AUO B140XTN07.5
    00 ff ff ff ff ff ff 00 06 af 90 02 00 00 00 00
    00 1e 01 04 95 1f 11 78 03 c0 d5 8f 56 58 93 29
    20 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 ce 1d 56 e2 50 00 1e 30 26 16
    36 00 35 ad 10 00 00 18 df 13 56 e2 50 00 1e 30
    26 16 36 00 35 ad 10 00 00 18 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02
    00 10 48 ff 0f 3c 7d 48 0f 1b 7d 20 20 20 00 09
    
    AUO B140HAK03.5
    00 ff ff ff ff ff ff 00 06 af 9f 3c 00 00 00 00
    00 1f 01 04 95 1f 11 78 03 f5 65 8f 55 5a 93 2a
    1f 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 b0 36 80 a0 70 38 24 40 10 10
    3e 00 35 ae 10 00 00 18 75 24 80 a0 70 38 24 40
    10 10 3e 00 35 ae 10 00 00 18 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02
    00 10 48 ff 0f 3c 7d 14 0e 1e 7d 20 20 20 01 02
    
    70 20 79 02 00 22 00 14 df 22 02 84 7f 07 9f 00
    0f 80 0f 00 37 04 23 00 02 00 0d 00 25 00 09 df
    22 02 df 22 02 28 3c 80 81 00 10 72 1a 00 00 03
    01 28 3c 00 00 60 50 60 50 3c 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 3f 90
    
    AUO B116XTN02.3
    00 ff ff ff ff ff ff 00 06 af ba 49 00 00 00 00
    00 23 01 04 95 1a 0e 78 02 6b f5 91 55 54 91 27
    22 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 ce 1d 56 e2 50 00 1e 30 26 16
    36 00 00 90 10 00 00 18 df 13 56 e2 50 00 1e 30
    26 16 36 00 00 90 10 00 00 18 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02
    00 10 48 ff 0f 3c 7d 50 05 18 7d 20 20 20 00 7f
    
    AUO B140XTK02.4
    00 ff ff ff ff ff ff 00 06 af a8 67 00 00 00 00
    28 20 01 04 95 1f 11 78 03 c0 d5 8f 56 58 93 29
    20 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 ce 1d 56 e2 50 00 1e 30 26 16
    36 00 35 ad 10 00 00 18 df 13 56 e2 50 00 1e 30
    26 16 36 00 35 ad 10 00 00 18 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02
    00 10 48 ff 0f 3c 7d 11 23 21 6e 20 20 20 00 8e
    
    AUO B140HAN07.7
    00 ff ff ff ff ff ff 00 06 af ad c7 00 00 00 00
    21 21 01 04 a5 1f 11 78 03 fa 95 92 56 5a 92 2a
    20 50 54 00 00 00 01 01 01 01 01 01 01 01 01 01
    01 01 01 01 01 01 56 37 80 bc 70 38 28 40 10 10
    3e 00 35 ae 10 00 00 18 e4 24 80 bc 70 38 28 40
    10 10 3e 00 35 ae 10 00 00 18 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 02
    00 10 3c ff 0f 3c 96 23 10 28 96 20 20 20 01 66
    
    70 20 79 02 00 22 00 14 5b 29 02 84 7f 07 bb 00
    0f 80 0f 00 37 04 27 00 02 00 0d 00 25 01 09 5b
    29 02 5b 29 02 28 3c 80 81 00 10 6f 1a 00 00 03
    01 28 3c 00 00 8b 11 8b 11 3c 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00
    00 00 00 00 00 00 00 00 00 00 00 00 00 00 c0 90
    
    Signed-off-by: Terry Hsiao <terry_hsiao@compal.corp-partner.google.com>
    Link:
    https://patch.msgid.link/20260506170607.10813-2-terry_hsiao@compal.corp-partner.google.com
    
    (cherry picked from commit 4c34cdb93ea187b46d287a77a8946268a7d24286
    https://gitlab.freedesktop.org/drm/misc/kernel.git drm-misc-next)
    
    BUG=b:510983610
    TEST=chromeos-kernel-5_15 on corsola
    
    Change-Id: I9a1b5cb9f4a1ec214d5ccb831cc0c82332b767fa
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7839140
    Reviewed-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Tested-by: Terry Hsiao <terry_hsiao@compal.corp-partner.google.com>
    Reviewed-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-by: Shawn Ku <shawnku@chromium.org>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Shawn Ku <shawnku@chromium.org>

--- drivers/gpu/drm/panel/panel-edp.c
+++ drivers/gpu/drm/panel/panel-edp.c
@@ -2019,6 +2019,7 @@ static const struct panel_delay delay_200_500_e250 = {
  * Sort first by vendor, then by product ID.
  */
 static const struct edp_panel_entry edp_panels[] = {
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x0290, &delay_200_500_e50, "B140XTN07.5"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x105c, &delay_200_500_e50, "B116XTN01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x1062, &delay_200_500_e50, "B120XAN01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x125c, &delay_200_500_e50, "Unknown"),
@@ -2031,21 +2032,25 @@ static const struct edp_panel_entry edp_panels[] = {
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x235c, &delay_200_500_e50, "B116XTN02.3"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x239b, &delay_200_500_e50, "B116XAN06.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x255c, &delay_200_500_e50, "B116XTN02.5"),
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x3c9f, &delay_200_500_e50, "B140HAK03.5"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x403d, &delay_200_500_e50, "B140HAN04.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x405c, &auo_b116xak01.delay, "B116XAN04.0"),
 	EDP_PANEL_ENTRY2('A', 'U', 'O', 0x405c, &auo_b116xak01.delay, "B116XAK01.0",
 			 &auo_b116xa3_mode),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x435c, &delay_200_500_e50, "Unknown"),
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x49ba, &delay_200_500_e50, "B116XTN02.3"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x582d, &delay_200_500_e50, "B133UAN01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x615c, &delay_200_500_e50, "B116XAN06.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x635c, &delay_200_500_e50, "B116XAN06.3"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x639c, &delay_200_500_e50, "B140HAK02.7"),
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0x67a8, &delay_200_500_e50, "B140XTK02.4"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x723c, &delay_200_500_e50, "B140XTN07.2"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x73aa, &delay_200_500_e50, "B116XTN02.3"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x8594, &delay_200_500_e50, "B133UAN01.0"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0x89ba, &delay_200_500_e50, "B116XAT04.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0xa199, &delay_200_500_e50, "B116XAN06.1"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0xc4b4, &delay_200_500_e50, "B116XAT04.1"),
+	EDP_PANEL_ENTRY('A', 'U', 'O', 0xc7ad, &delay_200_500_e50, "B140HAN07.7"),
 	EDP_PANEL_ENTRY('A', 'U', 'O', 0xf390, &delay_200_500_e50, "B140XTN07.7"),
 
 	EDP_PANEL_ENTRY('B', 'O', 'E', 0x0607, &delay_200_500_e200, "Unknown"),
