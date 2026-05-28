a0946649c1ea2bbbc87003d57959312d1d7f851e
Tzung-Bi Shih <tzungbi@chromium.org>
2026-05-28 02:32:52 +0000

    Revert "FROMLIST: platform/chrome: Protect cros_ec_device lifecycle with revocable"
    This reverts commit 3d8642e5d350b4ef7643be0559c3b5db6cc04631.
    
    Reason: Re-applying upstream solution.
    
    BUG=b:228118385
    BUG=b:443614159
    TEST=run test program in b/228118385#comment9 on brya.
    
    Change-Id: Icfd08ca1040cd29587aeca44bf071a1c71536aa6
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7878367
    Reviewed-by: Tomasz Figa <tfiga@chromium.org>

--- include/linux/platform_data/cros_ec_proto.h
+++ include/linux/platform_data/cros_ec_proto.h
@@ -12,7 +12,6 @@
 #include <linux/lockdep_types.h>
 #include <linux/mutex.h>
 #include <linux/notifier.h>
-#include <linux/revocable.h>
 
 #include <linux/platform_data/cros_ec_commands.h>
 
@@ -159,7 +158,6 @@ struct cros_ec_command {
  * @pd: The platform_device used by the mfd driver to interface with the
  *      PD behind an EC.
  * @panic_notifier: EC panic notifier.
- * @revocable_provider: The revocable_provider to this device.
  */
 struct cros_ec_device {
 	/* These are used by other drivers that want to talk to the EC */
@@ -205,8 +203,6 @@ struct cros_ec_device {
 	struct platform_device *pd;
 
 	struct blocking_notifier_head panic_notifier;
-
-	struct revocable_provider *revocable_provider;
 };
 
 /**
