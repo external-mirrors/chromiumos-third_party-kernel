501146ead4a998eeb106cd0fda7e6b1536657129
Tomasz Figa <tfiga@chromium.org>
2026-06-17 16:32:39 +0900

    Revert "FROMLIST: wifi: mt76: mt7921: add auto regdomain switch support"
    This reverts commit 00fbf9feacac5bf74e4cf10ebf69996c80b2cd3d.
    
    Reason for revert: Causes a deadlock
    
    Failure Link: b/520460919
    
    Original change's description:
    > FROMLIST: wifi: mt76: mt7921: add auto regdomain switch support
    >
    > Implement 802.11d-based automatic regulatory domain switching to
    > dynamically determine the regulatory domain at runtime.
    >
    > The scan-done event structure by reusing reserved padding and appending
    > new fields; the layout and values remains backward-compatible with
    > existing users.
    >
    > Co-developed-by: Sean Wang <sean.wang@mediatek.com>
    > Signed-off-by: Sean Wang <sean.wang@mediatek.com>
    > Signed-off-by: JB Tsai <jb.tsai@mediatek.com>
    > (am from https://patchwork.kernel.org/project/linux-wireless/patch/20260303053637.465465-4-jb.tsai@mediatek.com/)
    > (also found at https://lore.kernel.org/r/20260303053637.465465-4-jb.tsai@mediatek.com)
    >
    > UPSTREAM-TASK=b:491671758
    > BUG=b:477679268
    > TEST=test AVL on skywalker
    >
    > Change-Id: I02894b7ffd27b70abe5c26c005aa72b3fe19b02a
    > Signed-off-by: JB Tsai <jb.tsai@mediatek.corp-partner.google.com>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7636919
    > Reviewed-by: Jintao Lin <jintaolin@chromium.org>
    > Reviewed-by: David Ruth <druth@chromium.org>
    > Reviewed-by: Sean Paul <sean@poorly.run>
    > Commit-Queue: David Ruth <druth@chromium.org>
    > Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    > Tested-by: David Ruth <druth@chromium.org>
    
    BUG=b:477679268
    BUG=b:520460919
    
    Change-Id: Ic5015ae53614478f40fec3b4b500b3613f092817
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7955537
    Reviewed-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    (cherry picked from commit fd1624291169cb85ddc79f2df20de32578752d45)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/7961857

--- drivers/net/wireless/mediatek/mt76/mt7921/mcu.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/mcu.c
@@ -478,8 +478,7 @@ static int mt7921_load_clc(struct mt792x_dev *dev, const char *fw_name)
 			goto out;
 		}
 	}
-
-	ret = mt7921_regd_init(phy);
+	ret = mt7921_mcu_set_clc(dev, "00", ENVIRON_INDOOR);
 out:
 	release_firmware(fw);
 
