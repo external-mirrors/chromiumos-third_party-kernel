f1a2105274066e306c2914b297e563539de698e2
Tzung-Bi Shih <tzungbi@chromium.org>
2026-09-16 04:27:10 +0000

    Revert "HID: pass the buffer size to hid_report_raw_event"
    This reverts commit 9e36568e67f817c728f9d79049d212da79109a75.
    
    Reason: the series introduces a regression.
    
    BUG=b:561664220
    BUG=b:532406913
    TEST=cq
    
    Change-Id: Ic10e071d694c6cb3b5aea1ab3e08bcff3cc8df30
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/8411790
    Tested-by: Jędrzej Ciupis <jciupis@google.com>
    Reviewed-by: Jędrzej Ciupis <jciupis@google.com>
    (cherry picked from commit b171e008fce9509402a10870fce1cd4de75983a5)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/8410030

--- drivers/hid/bpf/hid_bpf_dispatch.c
+++ drivers/hid/bpf/hid_bpf_dispatch.c
@@ -47,7 +47,7 @@ __weak noinline int hid_bpf_device_event(struct hid_bpf_ctx *ctx)
 
 u8 *
 dispatch_hid_bpf_device_event(struct hid_device *hdev, enum hid_report_type type, u8 *data,
-			      size_t *buf_size, u32 *size, int interrupt)
+			      u32 *size, int interrupt)
 {
 	struct hid_bpf_ctx_kern ctx_kern = {
 		.ctx = {
@@ -81,7 +81,6 @@ dispatch_hid_bpf_device_event(struct hid_device *hdev, enum hid_report_type type
 		*size = ret;
 	}
 
-	*buf_size = ctx_kern.ctx.allocated_size;
 	return ctx_kern.data;
 }
 EXPORT_SYMBOL_GPL(dispatch_hid_bpf_device_event);
